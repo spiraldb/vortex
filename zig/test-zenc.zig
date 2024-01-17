d66f47c8002281d5f7a0abcb94dcf619c8b972f0
Will Manning <will@fulcrum.so>
2024-01-16 17:08:27 -0800

    zig test of calling statically linked codecz over C ABI (#629)

--- zig/test-zenc.zig
+++ zig/test-zenc.zig
@@ -29,3 +29,29 @@ test "RunLengthStats_t conversion" {
     try std.testing.expectEqual(cti.Struct.fields[0], zti.Struct.fields[0]);
     try std.testing.expectEqual(cti.Struct.fields[1], zti.Struct.fields[1]);
 }
+
+test "extern ree" {
+    const ints = [_]i32{ 1, 1, 1, 2, 3, 4, 4, 5 };
+    const numRuns = 5;
+
+    var valuesOut: [5]i32 = [_]i32{0} ** 5;
+    const valuesBuf = c.ByteBuffer_t{ .ptr = @ptrCast(&valuesOut), .len = valuesOut.len * @sizeOf(i32) };
+    var runEndsOut: [5]u32 = [_]u32{0} ** 5;
+    const runEndsBuf = c.ByteBuffer_t{ .ptr = @ptrCast(&runEndsOut), .len = runEndsOut.len * @sizeOf(u32) };
+
+    const result = c.codecz_ree_encode_i32_u32(@ptrCast(&ints), ints.len, valuesBuf, runEndsBuf);
+
+    try std.testing.expectEqual(result.status, c.Ok);
+    try std.testing.expectEqualDeep(result.firstBuffer.buffer, valuesBuf);
+    try std.testing.expectEqualDeep(result.secondBuffer.buffer, runEndsBuf);
+    try std.testing.expectEqual(result.firstBuffer.numElements, numRuns);
+    try std.testing.expectEqual(result.secondBuffer.numElements, numRuns);
+    try std.testing.expectEqual(result.firstBuffer.inputBytesUsed, valuesOut.len * @sizeOf(i32));
+    try std.testing.expectEqual(result.secondBuffer.inputBytesUsed, runEndsOut.len * @sizeOf(u32));
+
+    const values = [_]i32{ 1, 2, 3, 4, 5 };
+    try std.testing.expectEqualSlices(i32, &values, &valuesOut);
+
+    const runEnds = [_]u32{ 3, 4, 5, 7, 8 };
+    try std.testing.expectEqualSlices(u32, &runEnds, &runEndsOut);
+}
