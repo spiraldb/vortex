6092b741281706d6de381c492382c13bbf3e8732
Will Manning <will@fulcrum.so>
2024-01-31 03:32:01 -0500

    remove trazy (#746)
    we're not using it. we can bring it back later by reverting this PR if
    we want it back

--- build.zig
+++ build.zig
@@ -14,25 +14,6 @@ pub fn build(b: *std.Build) void {
     const filter_test = b.option([]const u8, "filter-tests", "Filter tests by name");
     const test_step = b.step("test", "Run library tests");
 
-    // tracy options
-    const tracy = b.option(bool, "tracy", "Enable Tracy integration") orelse true;
-    const tracy_callstack = b.option(bool, "tracy-callstack", "Include callstack information with Tracy data. Does nothing if -Dtracy is not provided") orelse tracy;
-    const tracy_allocation = b.option(bool, "tracy-allocation", "Include allocation information with Tracy data. Does nothing if -Dtracy is not provided") orelse tracy;
-    const callstack_depth = b.option(u16, "tracy-callstack-depth", "Depth of tracy callstack information. 10 by default") orelse 10;
-    const tracyOpts = b.addOptions();
-    tracyOpts.addOption(bool, "enable_tracy", tracy);
-    tracyOpts.addOption(bool, "enable_tracy_callstack", tracy_callstack);
-    tracyOpts.addOption(bool, "enable_tracy_allocation", tracy_allocation);
-    tracyOpts.addOption(u16, "callstack_depth", callstack_depth);
-
-    // trazy
-    const trazy = b.addModule("trazy", CreateOptions{
-        .root_source_file = .{ .path = "zig/trazy/trazy.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    trazy.addImport("tracy_options", tracyOpts.createModule());
-
     // zimd
     const zimd = b.addModule("zimd", CreateOptions{
         .root_source_file = .{ .path = "zig/zimd/zimd.zig" },
@@ -71,7 +52,6 @@ pub fn build(b: *std.Build) void {
         .link_libc = true,
     });
     codecz.addImport("abi", c_abi_types);
-    // codecz.addImport("trazy", trazy);
     codecz.addImport("zimd", zimd);
 
     // test codecs
@@ -85,8 +65,6 @@ pub fn build(b: *std.Build) void {
     codecz_test.root_module.addImport("codecz", codecz);
     codecz_test.root_module.addImport("abi", c_abi_types);
     codecz_test.root_module.addImport("zimd", zimd);
-    // codecz_test.root_module.addImport("trazy", trazy);
-    // dependencyTracy(codecz_test);
     codecz_test.addIncludePath(.{ .path = "zig/c-abi/include" });
     const codecz_test_run = b.addRunArtifact(codecz_test);
     test_step.dependOn(&codecz_test_run.step);
@@ -105,8 +83,6 @@ pub fn build(b: *std.Build) void {
     lib_step.root_module.addImport("codecz", codecz);
     lib_step.root_module.addImport("abi", c_abi_types);
     lib_step.root_module.addImport("zimd", zimd);
-    //lib_step.root_module.addImport("trazy", trazy);
-    //dependencyTracy(lib_step);
     lib_step.addIncludePath(.{ .path = "zig/c-abi/include" });
     lib_step.root_module.c_std = std.Build.CStd.C11;
     lib_step.bundle_compiler_rt = true;
@@ -135,13 +111,4 @@ pub fn build(b: *std.Build) void {
         const test_bin_install = b.addInstallBinFile(codecz_test.getEmittedBin(), "test.bin");
         b.getInstallStep().dependOn(&test_bin_install.step);
     }
-}
-
-fn dependencyTracy(lib: *std.Build.Step.Compile) void {
-    lib.linkLibCpp();
-    lib.addIncludePath(.{ .path = "zig/deps/tracy/public/tracy" });
-    lib.addCSourceFile(.{
-        .file = .{ .path = "zig/deps/tracy/public/TracyClient.cpp" },
-        .flags = &.{ "-fno-sanitize=undefined", "-DTRACY_ENABLE=1" },
-    });
 }
