feaab609489456f2ec196bf52b37e01594052c4b
Will Manning <will@fulcrum.so>
2024-01-31 03:53:51 -0500

    delete enc-legacy and pyenc-legacy (#747)
    and update zig/README.md accordingly

--- zig/enc-legacy/compute/ops.zig
+++
@@ -1,7 +0,0 @@
-pub usingnamespace @import("./ops/add.zig");
-pub usingnamespace @import("./ops/all.zig");
-pub usingnamespace @import("./ops/compress.zig");
-// pub usingnamespace @import("./ops/cumsum.zig");
-pub usingnamespace @import("./ops/max.zig");
-pub usingnamespace @import("./ops/min.zig");
-pub usingnamespace @import("./ops/take.zig");
