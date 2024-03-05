2824169fe0a99a1d896043b824d90ff6ededd8a1
Nicholas Gates <nick@nickgates.com>
2024-03-05 17:28:42 +0000

    Bench

--- vortex-alp/benches/alp_compress.rs
+++ vortex-alp/benches/alp_compress.rs
@@ -1,4 +1,6 @@
-use vortex_alp::{ALPFloat, Exponents};
+use vortex::array::primitive::PrimitiveArray;
+use vortex::array::ArrayRef;
+use vortex_alp::{ALPArray, ALPFloat, Exponents};
 
 fn main() {
     divan::main();
@@ -8,4 +10,11 @@ fn main() {
 fn alp_compress<T: ALPFloat>(n: usize) -> (Exponents, Vec<T::ALPInt>, Vec<u64>, Vec<T>) {
     let values: Vec<T> = vec![T::from(1.234).unwrap(); n];
     T::encode(values.as_slice(), None)
+}
+
+// TODO(ngates): remove this
+#[divan::bench(args = [100_000, 10_000_000])]
+fn alp_compress_array(n: usize) -> ArrayRef {
+    let array = PrimitiveArray::from_vec(vec![1.234f64; n]);
+    ALPArray::encode(&array).unwrap()
 }
