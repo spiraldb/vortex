dcbaf8567db01c52fb167d2e2ced8222bee2ebc3
Will Manning <will@willmanning.io>
2024-03-19 17:16:21 +0000

    consolidate

--- vortex-alp/src/compute.rs
+++ vortex-alp/src/compute.rs
@@ -1,10 +1,29 @@
-use std::f32;
 use vortex::array::Array;
+use vortex::compute::ArrayCompute;
+use vortex::compute::flatten::{FlattenedArray, FlattenFn};
 use vortex::compute::scalar_at::{scalar_at, ScalarAtFn};
 use vortex::dtype::{DType, FloatWidth};
 use vortex::error::{VortexError, VortexResult};
 use vortex::scalar::Scalar;
+
 use crate::{ALPArray, ALPFloat};
+use crate::compress::decompress;
+
+impl ArrayCompute for ALPArray {
+    fn flatten(&self) -> Option<&dyn FlattenFn> {
+        Some(self)
+    }
+
+    fn scalar_at(&self) -> Option<&dyn ScalarAtFn> {
+        Some(self)
+    }
+}
+
+impl FlattenFn for ALPArray {
+    fn flatten(&self) -> VortexResult<FlattenedArray> {
+        decompress(self).map(FlattenedArray::Primitive)
+    }
+}
 
 impl ScalarAtFn for ALPArray {
     fn scalar_at(&self, index: usize) -> VortexResult<Scalar> {
