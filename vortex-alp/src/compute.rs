c6674ee46f4db74a4af28c5f650ad0ad43ab4881
Will Manning <will@willmanning.io>
2024-03-20 13:09:35 +0000

    flatten ALP arrays (#123)
    * wip
    
    * flatten
    
    * consolidate
    
    * asdfasd
    
    * fixes
    
    * tests and fixes
    
    * remove extra
    
    * lints

--- vortex-alp/src/compute.rs
+++ vortex-alp/src/compute.rs
@@ -1,16 +1,26 @@
-use crate::alp::ALPFloat;
-use crate::ALPArray;
-use std::f32;
 use vortex::array::Array;
+use vortex::compute::flatten::{FlattenFn, FlattenedArray};
 use vortex::compute::scalar_at::{scalar_at, ScalarAtFn};
 use vortex::compute::ArrayCompute;
-use vortex::dtype::{DType, FloatWidth};
-use vortex::error::{VortexError, VortexResult};
+use vortex::error::VortexResult;
 use vortex::scalar::Scalar;
 
+use crate::compress::decompress;
+use crate::{match_each_alp_float_ptype, ALPArray, ALPFloat};
+
 impl ArrayCompute for ALPArray {
+    fn flatten(&self) -> Option<&dyn FlattenFn> {
+        Some(self)
+    }
+
     fn scalar_at(&self) -> Option<&dyn ScalarAtFn> {
         Some(self)
+    }
+}
+
+impl FlattenFn for ALPArray {
+    fn flatten(&self) -> VortexResult<FlattenedArray> {
+        decompress(self).map(FlattenedArray::Primitive)
     }
 }
 
@@ -21,23 +31,12 @@ impl ScalarAtFn for ALPArray {
         }
 
         let encoded_val = scalar_at(self.encoded(), index)?;
-
-        match self.dtype() {
-            DType::Float(FloatWidth::_32, _) => {
-                let encoded_val: i32 = encoded_val.try_into().unwrap();
-                Ok(Scalar::from(<f32 as ALPFloat>::decode_single(
-                    encoded_val,
-                    self.exponents(),
-                )))
-            }
-            DType::Float(FloatWidth::_64, _) => {
-                let encoded_val: i64 = encoded_val.try_into().unwrap();
-                Ok(Scalar::from(<f64 as ALPFloat>::decode_single(
-                    encoded_val,
-                    self.exponents(),
-                )))
-            }
-            _ => Err(VortexError::InvalidDType(self.dtype().clone())),
-        }
+        match_each_alp_float_ptype!(self.dtype().try_into().unwrap(), |$T| {
+            let encoded_val: <$T as ALPFloat>::ALPInt = encoded_val.try_into().unwrap();
+            Scalar::from(<$T as ALPFloat>::decode_single(
+                encoded_val,
+                self.exponents(),
+            ))
+        })
     }
 }
