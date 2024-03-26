6f44e2fe0eaa5a5e9ea1d7a667c0f58a1c4792d1
Robert Kruszewski <github@robertk.io>
2024-03-26 18:45:27 +0000

    some

--- vortex-fastlanes/src/for/compute.rs
+++ vortex-fastlanes/src/for/compute.rs
@@ -1,16 +1,22 @@
-use crate::r#for::compress::decompress;
-use crate::FoRArray;
 use vortex::array::{Array, ArrayRef};
-use vortex::compute::flatten::{FlattenFn, FlattenedArray};
-use vortex::compute::take::{take, TakeFn};
 use vortex::compute::ArrayCompute;
+use vortex::compute::flatten::{FlattenedArray, FlattenFn};
+use vortex::compute::scalar_at::{scalar_at, ScalarAtFn};
+use vortex::compute::take::{take, TakeFn};
+use vortex::scalar::Scalar;
 use vortex_error::VortexResult;
+
+use crate::FoRArray;
+use crate::r#for::compress::decompress;
 
 impl ArrayCompute for FoRArray {
     fn flatten(&self) -> Option<&dyn FlattenFn> {
         Some(self)
     }
 
+    fn scalar_at(&self) -> Option<&dyn ScalarAtFn> {
+    Some(self)
+}
     fn take(&self) -> Option<&dyn TakeFn> {
         Some(self)
     }
@@ -29,6 +35,14 @@ impl TakeFn for FoRArray {
             self.reference.clone(),
             self.shift,
         )?
-        .into_array())
+            .into_array())
+    }
+}
+
+impl ScalarAtFn for FoRArray {
+    fn scalar_at(&self, index: usize) -> VortexResult<Scalar> {
+        let encoded_scalar: usize = scalar_at(self.encoded(), index)?.try_into()?;
+        let reference: usize = self.reference().try_into()?;
+        Ok(encoded_scalar >> self.shift() + reference).into())
     }
 }
