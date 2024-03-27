4ea1fec536c37db675824fc6c9401cc03bbdb6b6
Nicholas Gates <nick@nickgates.com>
2024-03-27 09:23:55 +0000

    Fix up tests

--- vortex-roaring/src/boolean/compute.rs
+++ vortex-roaring/src/boolean/compute.rs
@@ -1,13 +1,39 @@
+use arrow_buffer::{BooleanBuffer, Buffer};
+use vortex::array::bool::BoolArray;
+use vortex::compute::flatten::{FlattenFn, FlattenedArray};
 use vortex::compute::scalar_at::ScalarAtFn;
 use vortex::compute::ArrayCompute;
-use vortex::scalar::Scalar;
+use vortex::scalar::{AsBytes, Scalar};
+use vortex::validity::ArrayValidity;
 use vortex_error::VortexResult;
 
 use crate::RoaringBoolArray;
 
 impl ArrayCompute for RoaringBoolArray {
+    fn flatten(&self) -> Option<&dyn FlattenFn> {
+        Some(self)
+    }
+
     fn scalar_at(&self) -> Option<&dyn ScalarAtFn> {
         Some(self)
+    }
+}
+
+impl FlattenFn for RoaringBoolArray {
+    fn flatten(&self) -> VortexResult<FlattenedArray> {
+        // TODO(ngates): benchmark the fastest conversion from BitMap.
+        //  Via bitset requires two copies.
+        let bitset = self
+            .bitmap
+            .to_bitset()
+            .ok_or("Failed to convert RoaringBitmap to Bitset")?;
+
+        let bytes = &bitset.as_slice().as_bytes()[0..bitset.size_in_bytes()];
+        let buffer = Buffer::from_slice_ref(bytes);
+        Ok(FlattenedArray::Bool(BoolArray::new(
+            BooleanBuffer::new(buffer, 0, bitset.size_in_bits()),
+            self.validity(),
+        )))
     }
 }
 
