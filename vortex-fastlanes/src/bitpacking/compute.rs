4b8dfc798468f0bca86c9402915fab6b3b0eb707
Will Manning <will@willmanning.io>
2024-04-05 12:15:22 -0400

    use sparse take (currently broken)

--- vortex-fastlanes/src/bitpacking/compute.rs
+++ vortex-fastlanes/src/bitpacking/compute.rs
@@ -1,21 +1,20 @@
-use std::collections::HashMap;
+use itertools::Itertools;
 
 use fastlanez::TryBitPack;
-use itertools::Itertools;
+use vortex::array::{Array, ArrayRef};
 use vortex::array::downcast::DowncastArrayBuiltin;
 use vortex::array::primitive::PrimitiveArray;
-use vortex::array::{Array, ArrayRef};
-use vortex::compute::flatten::{flatten_primitive, FlattenFn, FlattenedArray};
+use vortex::compute::ArrayCompute;
+use vortex::compute::flatten::{flatten_primitive, FlattenedArray, FlattenFn};
 use vortex::compute::scalar_at::{scalar_at, ScalarAtFn};
 use vortex::compute::take::{take, TakeFn};
-use vortex::compute::ArrayCompute;
 use vortex::match_each_integer_ptype;
 use vortex::ptype::NativePType;
 use vortex::scalar::Scalar;
 use vortex_error::{vortex_bail, vortex_err, VortexResult};
 
+use crate::{BitPackedArray, match_integers_by_width, unpack_single_primitive};
 use crate::bitpacking::compress::{unpack, unpack_single};
-use crate::{match_integers_by_width, unpack_single_primitive, BitPackedArray};
 
 impl ArrayCompute for BitPackedArray {
     fn flatten(&self) -> Option<&dyn FlattenFn> {
@@ -111,33 +110,23 @@ fn take_primitive<T: NativePType + TryBitPack>(
     }
 
     if let Some(patches) = array.patches() {
-        if let Some(patches) = patches.maybe_sparse() {
-            // we first find which of the taken indices need to be patched
-            let indices_map: HashMap<u64, u64> = patches
-                .resolved_indices()
-                .iter()
-                .enumerate()
-                .map(|(i, r)| (*r as u64, i as u64))
-                .collect();
-            let (output_indices, patch_indices): (Vec<usize>, Vec<u64>) = match_each_integer_ptype!(indices.ptype(), |$P| {
-                indices.typed_data::<$P>()
-                    .iter()
-                    .map(|i| *i as u64)
-                    .enumerate()
-                    .filter_map(|(i, pi)| indices_map.get(&pi).map(|ri| (i, *ri)))
-                    .unzip()
-            });
-            let patch_indices = PrimitiveArray::from(patch_indices);
+        let taken_patches = patches
+            .maybe_sparse()
+            .map(|patches| take(patches, indices))
+            .transpose()?
+            .ok_or(vortex_err!("Only sparse patches are currently supported!"))?;
+        let taken_patches = taken_patches
+            .maybe_sparse()
+            .ok_or(vortex_err!("Only sparse patches are currently supported!"))?;
 
-            // then we `take` the patched values and apply to our output
-            let patch_values = flatten_primitive(&take(patches.values(), &patch_indices)?)?;
-            output_indices
-                .iter()
-                .zip(patch_values.typed_data::<T>())
-                .for_each(|(i, v)| output[*i] = *v)
-        } else {
-            vortex_bail!("Only sparse patches are currently supported!");
-        }
+        let output_patches = flatten_primitive(taken_patches.values())?;
+        taken_patches
+            .resolved_indices()
+            .iter()
+            .zip_eq(output_patches.typed_data::<T>())
+            .for_each(|(idx, val)| {
+                output[*idx] = *val;
+            });
     }
     Ok(output)
 }
@@ -147,19 +136,20 @@ mod test {
     use std::sync::Arc;
 
     use itertools::Itertools;
+    use rand::{Rng, thread_rng};
     use rand::distributions::Uniform;
-    use rand::{thread_rng, Rng};
+
+    use vortex::array::Array;
     use vortex::array::downcast::DowncastArrayBuiltin;
     use vortex::array::primitive::{PrimitiveArray, PrimitiveEncoding};
-    use vortex::array::Array;
     use vortex::compress::{CompressConfig, CompressCtx, EncodingCompression};
     use vortex::compute::scalar_at::scalar_at;
     use vortex::compute::take::take;
     use vortex::encoding::EncodingRef;
     use vortex::scalar::Scalar;
 
-    use crate::downcast::DowncastFastlanes;
     use crate::BitPackedEncoding;
+    use crate::downcast::DowncastFastlanes;
 
     #[test]
     fn take_indices() {
