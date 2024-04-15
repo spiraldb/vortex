0fbb3587dacdf3ee2861e90d3e8c7b8bab74da7d
Nicholas Gates <nick@nickgates.com>
2024-04-15 18:05:55 +0100

    Sparse

--- vortex-array/src/scalar/serde.rs
+++ vortex-array/src/scalar/serde.rs
@@ -1,21 +1,115 @@
 use flatbuffers::{FlatBufferBuilder, WIPOffset};
-use num_enum::{IntoPrimitive, TryFromPrimitive};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use vortex_error::VortexError;
 use vortex_flatbuffers::{ReadFlatBuffer, WriteFlatBuffer};
 use vortex_schema::DTypeSerdeContext;
 
 use crate::flatbuffers::scalar as fb;
-use crate::scalar::Scalar;
+use crate::ptype::PType;
+use crate::scalar::{PScalar, Scalar};
 
 impl WriteFlatBuffer for Scalar {
     type Target<'a> = fb::Scalar<'a>;
 
     fn write_flatbuffer<'fb>(
         &self,
-        _fbb: &mut FlatBufferBuilder<'fb>,
+        fbb: &mut FlatBufferBuilder<'fb>,
     ) -> WIPOffset<Self::Target<'fb>> {
-        todo!()
+        let union = match self {
+            Scalar::Binary(b) => {
+                let bytes = b.value().map(|bytes| fbb.create_vector(bytes));
+                fb::ScalarArgs {
+                    type_type: fb::Type::Binary,
+                    type_: bytes.map(|b| {
+                        fb::Binary::create(fbb, &fb::BinaryArgs { value: Some(b) }).as_union_value()
+                    }),
+                }
+            }
+            Scalar::Bool(b) => fb::ScalarArgs {
+                type_type: fb::Type::Bool,
+                type_: b
+                    .value()
+                    .map(|&value| fb::Bool::create(fbb, &fb::BoolArgs { value }).as_union_value()),
+            },
+            Scalar::List(_) => panic!(),
+            Scalar::Null(_) => fb::ScalarArgs {
+                type_type: fb::Type::Null,
+                type_: None,
+            },
+            Scalar::Primitive(p) => {
+                // TODO(ngates): clean this up.
+                // We could probably just keep PScalar as PType + Option<[u8]> internally too?
+                let primitive = p
+                    .value()
+                    .map(|pscalar| match pscalar {
+                        PScalar::U8(v) => {
+                            let pbytes = fbb.create_vector(&v.to_le_bytes());
+                            fb::PrimitiveArgs {
+                                ptype: fb::PType::U8,
+                                bytes: Some(pbytes),
+                            }
+                        }
+                        _ => panic!(),
+                    })
+                    .unwrap_or_else(|| match p.ptype() {
+                        PType::U8 => fb::PrimitiveArgs {
+                            ptype: fb::PType::U8,
+                            bytes: None,
+                        },
+                        PType::U16 => fb::PrimitiveArgs {
+                            ptype: fb::PType::U16,
+                            bytes: None,
+                        },
+                        PType::U32 => fb::PrimitiveArgs {
+                            ptype: fb::PType::U32,
+                            bytes: None,
+                        },
+                        PType::U64 => fb::PrimitiveArgs {
+                            ptype: fb::PType::U64,
+                            bytes: None,
+                        },
+                        PType::I8 => fb::PrimitiveArgs {
+                            ptype: fb::PType::I8,
+                            bytes: None,
+                        },
+                        PType::I16 => fb::PrimitiveArgs {
+                            ptype: fb::PType::I16,
+                            bytes: None,
+                        },
+                        PType::I32 => fb::PrimitiveArgs {
+                            ptype: fb::PType::I32,
+                            bytes: None,
+                        },
+                        PType::I64 => fb::PrimitiveArgs {
+                            ptype: fb::PType::I64,
+                            bytes: None,
+                        },
+                        PType::F16 => fb::PrimitiveArgs {
+                            ptype: fb::PType::F16,
+                            bytes: None,
+                        },
+                        PType::F32 => fb::PrimitiveArgs {
+                            ptype: fb::PType::F32,
+                            bytes: None,
+                        },
+                        PType::F64 => fb::PrimitiveArgs {
+                            ptype: fb::PType::F64,
+                            bytes: None,
+                        },
+                    });
+
+                let primitive = fb::Primitive::create(fbb, &primitive);
+                fb::ScalarArgs {
+                    type_type: fb::Type::Primitive,
+                    type_: Some(primitive.as_union_value()),
+                }
+            }
+            Scalar::Struct(_) => panic!(),
+            Scalar::Utf8(_) => panic!(),
+            Scalar::Composite(_) => panic!(),
+        };
+
+        fb::Scalar::create(fbb, &union)
     }
 }
 
@@ -103,143 +197,3 @@ impl<'de> Deserialize<'de> for Scalar {
 //         }
 //     }
 // }
-
-// impl<'a, 'b> ScalarWriter<'a, 'b> {
-//     pub fn new(writer: &'b mut WriteCtx<'a>) -> Self {
-//         Self { writer }
-//     }
-//
-//     pub fn write(&mut self, scalar: &Scalar) -> VortexResult<()> {
-//         let mut fbb = FlatBufferBuilder::new();
-//
-//         self.writer
-//             .write_fixed_slice([ScalarTag::from(scalar).into()])?;
-//         let union = match scalar {
-//             Scalar::Binary(b) => {
-//                 let bytes = b.value().map(|bytes| fbb.create_vector(bytes));
-//                 ScalarArgs {
-//                     type_type: Type::Binary,
-//                     type_: bytes.map(|b| {
-//                         Binary::create(&mut fbb, &BinaryArgs { value: Some(b) }).as_union_value()
-//                     }),
-//                 }
-//             }
-//             Scalar::Bool(b) => ScalarArgs {
-//                 type_type: Type::Bool,
-//                 type_: b
-//                     .value()
-//                     .map(|value| Bool::create(&mut fbb, &BoolArgs { value }).as_union_value()),
-//             },
-//             Scalar::List(_) => panic!(),
-//             Scalar::Null(_) => ScalarArgs {
-//                 type_type: Type::Null,
-//                 type_: None,
-//             },
-//             Scalar::Primitive(p) => {
-//                 // TODO(ngates): clean this up.
-//                 // We could probably just keep PScalar as PType + Option<[u8]> internally too?
-//                 let primitive = p
-//                     .value()
-//                     .map(|pscalar| match pscalar {
-//                         PScalar::U8(v) => {
-//                             let pbytes = fbb.create_vector(&v.to_le_bytes());
-//                             PrimitiveArgs {
-//                                 ptype: scalar::PType::U8,
-//                                 bytes: Some(pbytes),
-//                             }
-//                         }
-//                         _ => panic!(),
-//                     })
-//                     .unwrap_or_else(|| match p.ptype() {
-//                         PType::U8 => PrimitiveArgs {
-//                             ptype: scalar::PType::U8,
-//                             bytes: None,
-//                         },
-//                         PType::U16 => PrimitiveArgs {
-//                             ptype: scalar::PType::U16,
-//                             bytes: None,
-//                         },
-//                         PType::U32 => PrimitiveArgs {
-//                             ptype: scalar::PType::U32,
-//                             bytes: None,
-//                         },
-//                         PType::U64 => PrimitiveArgs {
-//                             ptype: scalar::PType::U64,
-//                             bytes: None,
-//                         },
-//                         PType::I8 => PrimitiveArgs {
-//                             ptype: scalar::PType::I8,
-//                             bytes: None,
-//                         },
-//                         PType::I16 => PrimitiveArgs {
-//                             ptype: scalar::PType::I16,
-//                             bytes: None,
-//                         },
-//                         PType::I32 => PrimitiveArgs {
-//                             ptype: scalar::PType::I32,
-//                             bytes: None,
-//                         },
-//                         PType::I64 => PrimitiveArgs {
-//                             ptype: scalar::PType::I64,
-//                             bytes: None,
-//                         },
-//                         PType::F16 => PrimitiveArgs {
-//                             ptype: scalar::PType::F16,
-//                             bytes: None,
-//                         },
-//                         PType::F32 => PrimitiveArgs {
-//                             ptype: scalar::PType::F32,
-//                             bytes: None,
-//                         },
-//                         PType::F64 => PrimitiveArgs {
-//                             ptype: scalar::PType::F64,
-//                             bytes: None,
-//                         },
-//                     });
-//
-//                 let primitive = scalar::Primitive::create(&mut fbb, &primitive);
-//                 ScalarArgs {
-//                     type_type: Type::Primitive,
-//                     type_: Some(primitive.as_union_value()),
-//                 }
-//             }
-//             Scalar::Struct(_) => panic!(),
-//             Scalar::Utf8(_) => panic!(),
-//             Scalar::Composite(_) => panic!(),
-//         };
-//
-//         let scalar = scalar::Scalar::create(&mut fbb, &union);
-//         fbb.finish_minimal(scalar);
-//         let (vec, offset) = fbb.collapse();
-//         self.writer.write_slice(&vec[offset..])
-//     }
-// }
-
-#[derive(Copy, Clone, IntoPrimitive, TryFromPrimitive)]
-#[repr(u8)]
-enum ScalarTag {
-    Binary,
-    Bool,
-    List,
-    Null,
-    // TODO(robert): rename to primitive once we stop using enum for serialization
-    PrimitiveS,
-    Struct,
-    Utf8,
-    Composite,
-}
-
-impl From<&Scalar> for ScalarTag {
-    fn from(value: &Scalar) -> Self {
-        match value {
-            Scalar::Binary(_) => ScalarTag::Binary,
-            Scalar::Bool(_) => ScalarTag::Bool,
-            Scalar::List(_) => ScalarTag::List,
-            Scalar::Null(_) => ScalarTag::Null,
-            Scalar::Primitive(_) => ScalarTag::PrimitiveS,
-            Scalar::Struct(_) => ScalarTag::Struct,
-            Scalar::Utf8(_) => ScalarTag::Utf8,
-            Scalar::Composite(_) => ScalarTag::Composite,
-        }
-    }
-}
