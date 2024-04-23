00e91a5b04403fb39f8494a89b91f8a78c83339b
Josh Casale <joshua.d.casale@gmail.com>
2024-04-23 15:38:14 +0100

    nit

--- vortex-fastlanes/src/bitpacking/compress.rs
+++ vortex-fastlanes/src/bitpacking/compress.rs
@@ -27,7 +27,7 @@ impl EncodingCompression for BitPackedEncoding {
         _config: &CompressConfig,
     ) -> Option<&dyn EncodingCompression> {
         // Only support primitive arrays
-        let parray = array.as_primitive().unwrap();
+        let parray = array.as_primitive()?;
 
         // Only supports ints
         if !parray.ptype().is_int() {
