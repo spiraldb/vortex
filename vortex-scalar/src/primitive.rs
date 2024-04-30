c54e523fb8194d1ae52c1c5a478b1d6a87ac0bba
Josh Casale <joshua.d.casale@gmail.com>
2024-04-30 18:31:08 -0400

    is_zero

--- vortex-scalar/src/primitive.rs
+++ vortex-scalar/src/primitive.rs
@@ -3,6 +3,7 @@ use std::cmp::Ordering;
 use std::fmt::{Display, Formatter};
 use std::mem::size_of;
 
+use num_traits::identities::Zero;
 use vortex_dtype::half::f16;
 use vortex_dtype::{match_each_integer_ptype, match_each_native_ptype};
 use vortex_dtype::{DType, Nullability};
@@ -12,6 +13,7 @@ use vortex_error::{vortex_bail, vortex_err, VortexError, VortexResult};
 use crate::Scalar;
 
 pub trait PScalarType: NativePType + Into<PScalar> + TryFrom<PScalar, Error = VortexError> {}
+
 impl<T: NativePType + Into<PScalar> + TryFrom<PScalar, Error = VortexError>> PScalarType for T {}
 
 #[derive(Debug, Clone, PartialEq)]
@@ -235,7 +237,19 @@ impl PScalar {
     }
 
     pub fn is_zero(&self) -> bool {
-        !self.is_negative() && !self.is_positive()
+        match self {
+            PScalar::U8(v) => *v == 0,
+            PScalar::U16(v) => *v == 0,
+            PScalar::U32(v) => *v == 0,
+            PScalar::U64(v) => *v == 0,
+            PScalar::I8(v) => *v == 0,
+            PScalar::I16(v) => *v == 0,
+            PScalar::I32(v) => *v == 0,
+            PScalar::I64(v) => *v == 0,
+            PScalar::F16(v) => (*v).is_zero(),
+            PScalar::F32(v) => (*v).is_zero(),
+            PScalar::F64(v) => (*v).is_zero(),
+        }
     }
 }
 
