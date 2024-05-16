6295808434ddd9a5199e90a91495361c578f7b4a
Marko Bakovic <markobakovic95@gmail.com>
2024-05-16 09:37:22 +0100

    cleaner

--- vortex-scalar/src/serde/proto.rs
+++ vortex-scalar/src/serde/proto.rs
@@ -1,9 +1,9 @@
 #![cfg(feature = "proto")]
 
 use prost_types::value::Kind;
-use prost_types::Value;
+use prost_types::{ListValue, Struct, Value};
 use vortex_buffer::BufferString;
-use vortex_dtype::DType;
+use vortex_dtype::{DType, StructDType};
 use vortex_error::{vortex_bail, vortex_err, VortexError};
 
 use crate::pvalue::PValue;
@@ -30,13 +30,13 @@ impl TryFrom<&pb::Scalar> for Scalar {
             .as_ref()
             .ok_or_else(|| vortex_err!(InvalidSerde: "ScalarValue missing value"))?;
 
-        let value = try_from_scalar_value(&dtype, pb_value)?;
+        let value = try_from_value(&dtype, pb_value)?;
 
         Ok(Self { dtype, value })
     }
 }
 
-fn try_from_scalar_value(dtype: &DType, value: &Value) -> Result<ScalarValue, VortexError> {
+fn try_from_value(dtype: &DType, value: &Value) -> Result<ScalarValue, VortexError> {
     let kind = value
         .kind
         .as_ref()
@@ -49,38 +49,49 @@ fn try_from_scalar_value(dtype: &DType, value: &Value) -> Result<ScalarValue, Vo
         Kind::StringValue(v) => ScalarValue::BufferString(BufferString::from(v.clone())),
         Kind::ListValue(v) => {
             if let DType::List(elem_dtype, _) = dtype {
-                let mut values: Vec<ScalarValue> = Vec::new();
-
-                for elem in v.values.iter() {
-                    values.push(try_from_scalar_value(elem_dtype, elem)?);
-                }
-
-                return Ok(ScalarValue::List(values.into()));
+                try_from_list_value(elem_dtype, v)?
+            } else {
+                vortex_bail!(InvalidSerde: "Expected a list dtype, found {:?}", dtype);
             }
-
-            vortex_bail!(InvalidSerde: "Expected a list dtype, found {:?}", dtype);
         }
         Kind::StructValue(v) => {
             if let DType::Struct(sdt, _) = dtype {
-                let mut values: Vec<ScalarValue> = Vec::new();
-
-                for (field, field_dt) in sdt.names().iter().zip(sdt.dtypes().iter()) {
-                    if let Some((_, v)) =
-                        // Add field values in order defined by the struct dtype.
-                        v.fields.iter().find(|(f, _)| field.as_ref() == f.as_str())
-                    {
-                        values.push(try_from_scalar_value(field_dt, v)?);
-                    } else if field_dt.is_nullable() {
-                        values.push(ScalarValue::Null);
-                    } else {
-                        vortex_bail!(InvalidSerde: "Non-nullable struct field {} not found", field);
-                    }
-                }
-
-                return Ok(ScalarValue::List(values.into()));
+                try_from_struct_value(sdt, v)?
+            } else {
+                vortex_bail!(InvalidSerde: "Expected a struct dtype, found {:?}", dtype);
             }
-
-            vortex_bail!(InvalidSerde: "Expected a struct dtype, found {:?}", dtype);
         }
     })
+}
+
+fn try_from_list_value(elem_dtype: &DType, value: &ListValue) -> Result<ScalarValue, VortexError> {
+    let mut values = vec![];
+
+    for elem in value.values.iter() {
+        values.push(try_from_value(elem_dtype, elem)?);
+    }
+
+    Ok(ScalarValue::List(values.into()))
+}
+
+fn try_from_struct_value(dtype: &StructDType, value: &Struct) -> Result<ScalarValue, VortexError> {
+    let mut values = vec![];
+
+    for (field, field_dt) in dtype.names().iter().zip(dtype.dtypes().iter()) {
+        if let Some((_, v)) =
+            // Add field values in order defined by the struct dtype.
+            value
+                .fields
+                .iter()
+                .find(|(f, _)| field.as_ref() == f.as_str())
+        {
+            values.push(try_from_value(field_dt, v)?);
+        } else if field_dt.is_nullable() {
+            values.push(ScalarValue::Null);
+        } else {
+            vortex_bail!(InvalidSerde: "Non-nullable struct field {} not found", field);
+        }
+    }
+
+    Ok(ScalarValue::List(values.into()))
 }
