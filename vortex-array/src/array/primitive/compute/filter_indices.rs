08720d4183457ac69fd4be37f4f1c1544c5a2840
Josh Casale <joshua.d.casale@gmail.com>
2024-05-17 16:19:01 +0100

    use booleanbuffers (again), return boolarray

--- vortex-array/src/array/primitive/compute/filter_indices.rs
+++ vortex-array/src/array/primitive/compute/filter_indices.rs
@@ -1,49 +1,43 @@
 use std::ops::{BitAnd, BitOr};
 
 use arrow_buffer::BooleanBuffer;
-
-use itertools::Itertools;
 use vortex_dtype::{match_each_native_ptype, NativePType};
 use vortex_error::{vortex_bail, VortexResult};
 use vortex_expr::expressions::{Disjunction, Predicate, Value};
 use vortex_expr::operators::Operator;
 
+use crate::array::bool::BoolArray;
 use crate::array::primitive::PrimitiveArray;
 use crate::compute::filter_indices::FilterIndicesFn;
-use crate::validity::Validity;
 use crate::{Array, ArrayTrait, IntoArray};
 
 impl FilterIndicesFn for PrimitiveArray {
     fn filter_indices(&self, predicate: &Disjunction) -> VortexResult<Array> {
         let conjunction_indices = predicate.conjunctions.iter().flat_map(|conj| {
-            conj
-                .predicates
+            conj.predicates
                 .iter()
                 .map(|pred| indices_matching_predicate(self, pred).unwrap())
-                .reduce(|a, b| {
-                    a.bitand(&b)
-                })
+                .reduce(|a, b| a.bitand(&b))
         });
-        let present_buf = self.validity()
+        let present_buf = self
+            .validity()
             .to_logical(self.len())
             .to_present_null_buffer()?
             .into_inner();
 
-        let indices = conjunction_indices
-            .reduce(|a, b| {
-                a.bitor(&b)
-            })
-            .map(|bitset| {
-                bitset.bitand(&present_buf)
-            })
-            .map(|bitset| bitset.iter().map(|idx| idx as u64).collect_vec())
-            .unwrap_or_default();
-            
-        Ok(PrimitiveArray::from_vec(indices, Validity::AllValid).into_array())
+        let bitset = conjunction_indices
+            .reduce(|a, b| a.bitor(&b))
+            .map(|bitset| bitset.bitand(&present_buf))
+            .unwrap_or_else(|| BooleanBuffer::new_set(self.len()));
+
+        Ok(BoolArray::from(bitset).into_array())
     }
 }
 
-fn indices_matching_predicate(arr: &PrimitiveArray, predicate: &Predicate) -> VortexResult<BooleanBuffer> {
+fn indices_matching_predicate(
+    arr: &PrimitiveArray,
+    predicate: &Predicate,
+) -> VortexResult<BooleanBuffer> {
     if predicate.left.head().is_some() {
         vortex_bail!("Invalid path for primitive array")
     }
@@ -63,10 +57,12 @@ fn indices_matching_predicate(arr: &PrimitiveArray, predicate: &Predicate) -> Vo
     Ok(matching_idxs)
 }
 
-fn apply_predicate<T: NativePType, F: Fn(&T, &T) -> bool>(lhs: &[T], rhs: &T, f: F) -> BooleanBuffer {
-    let matches = lhs
-        .iter()
-        .map(|lhs| f(lhs, rhs));
+fn apply_predicate<T: NativePType, F: Fn(&T, &T) -> bool>(
+    lhs: &[T],
+    rhs: &T,
+    f: F,
+) -> BooleanBuffer {
+    let matches = lhs.iter().map(|lhs| f(lhs, rhs));
 
     BooleanBuffer::from_iter(matches)
 }
@@ -84,6 +80,7 @@ fn get_predicate<T: NativePType>(op: &Operator) -> fn(&T, &T) -> bool {
 
 #[cfg(test)]
 mod test {
+    use itertools::Itertools;
     use vortex_dtype::field_paths::FieldPathBuilder;
     use vortex_expr::expressions::{lit, Conjunction};
     use vortex_expr::field_paths::FieldPathOperations;
@@ -95,6 +92,16 @@ mod test {
         arr.filter_indices(&Disjunction {
             conjunctions: vec![conj],
         })
+    }
+
+    fn to_int_indices(filtered_primitive: BoolArray) -> Vec<u64> {
+        let filtered = filtered_primitive
+            .boolean_buffer()
+            .iter()
+            .enumerate()
+            .flat_map(|(idx, v)| if v { Some(idx as u64) } else { None })
+            .collect_vec();
+        filtered
     }
 
     #[test]
@@ -121,10 +128,10 @@ mod test {
                 predicates: vec![field.clone().lt(lit(5u32))],
             },
         )
-            .unwrap()
-            .flatten_primitive()
-            .unwrap();
-        let filtered = filtered_primitive.typed_data::<u64>();
+        .unwrap()
+        .flatten_bool()
+        .unwrap();
+        let filtered = to_int_indices(filtered_primitive);
         assert_eq!(filtered, [0u64, 1, 2, 3]);
 
         let filtered_primitive = apply_conjunctive_filter(
@@ -133,10 +140,10 @@ mod test {
                 predicates: vec![field.clone().gt(lit(5u32))],
             },
         )
-            .unwrap()
-            .flatten_primitive()
-            .unwrap();
-        let filtered = filtered_primitive.typed_data::<u64>();
+        .unwrap()
+        .flatten_bool()
+        .unwrap();
+        let filtered = to_int_indices(filtered_primitive);
         assert_eq!(filtered, [6u64, 7, 8, 10]);
 
         let filtered_primitive = apply_conjunctive_filter(
@@ -145,10 +152,10 @@ mod test {
                 predicates: vec![field.clone().eq(lit(5u32))],
             },
         )
-            .unwrap()
-            .flatten_primitive()
-            .unwrap();
-        let filtered = filtered_primitive.typed_data::<u64>();
+        .unwrap()
+        .flatten_bool()
+        .unwrap();
+        let filtered = to_int_indices(filtered_primitive);
         assert_eq!(filtered, [5u64]);
 
         let filtered_primitive = apply_conjunctive_filter(
@@ -157,10 +164,10 @@ mod test {
                 predicates: vec![field.clone().gte(lit(5u32))],
             },
         )
-            .unwrap()
-            .flatten_primitive()
-            .unwrap();
-        let filtered = filtered_primitive.typed_data::<u64>();
+        .unwrap()
+        .flatten_bool()
+        .unwrap();
+        let filtered = to_int_indices(filtered_primitive);
         assert_eq!(filtered, [5u64, 6, 7, 8, 10]);
 
         let filtered_primitive = apply_conjunctive_filter(
@@ -169,10 +176,10 @@ mod test {
                 predicates: vec![field.clone().lte(lit(5u32))],
             },
         )
-            .unwrap()
-            .flatten_primitive()
-            .unwrap();
-        let filtered = filtered_primitive.typed_data::<u64>();
+        .unwrap()
+        .flatten_bool()
+        .unwrap();
+        let filtered = to_int_indices(filtered_primitive);
         assert_eq!(filtered, [0u64, 1, 2, 3, 5]);
     }
 
@@ -187,10 +194,10 @@ mod test {
                 predicates: vec![field.clone().lt(lit(5u32)), field.clone().gt(lit(2u32))],
             },
         )
-            .unwrap()
-            .flatten_primitive()
-            .unwrap();
-        let filtered = filtered_primitive.typed_data::<u64>();
+        .unwrap()
+        .flatten_bool()
+        .unwrap();
+        let filtered = to_int_indices(filtered_primitive);
         assert_eq!(filtered, [2u64, 3])
     }
 
@@ -205,10 +212,10 @@ mod test {
                 predicates: vec![field.clone().lt(lit(5u32)), field.clone().gt(lit(5u32))],
             },
         )
-            .unwrap()
-            .flatten_primitive()
-            .unwrap();
-        let filtered = filtered_primitive.typed_data::<u64>();
+        .unwrap()
+        .flatten_bool()
+        .unwrap();
+        let filtered = to_int_indices(filtered_primitive);
         let expected: [u64; 0] = [];
         assert_eq!(filtered, expected)
     }
@@ -228,12 +235,8 @@ mod test {
         let disj = Disjunction {
             conjunctions: vec![c1, c2],
         };
-        let filtered_primitive = arr
-            .filter_indices(&disj)
-            .unwrap()
-            .flatten_primitive()
-            .unwrap();
-        let filtered = filtered_primitive.typed_data::<u64>();
+        let filtered_primitive = arr.filter_indices(&disj).unwrap().flatten_bool().unwrap();
+        let filtered = to_int_indices(filtered_primitive);
         assert_eq!(filtered, [0u64, 1, 2, 3, 5, 6, 7, 8, 9])
     }
 }
