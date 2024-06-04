7f83344f93eda5f4231e91b389f170217916090d
Robert Kruszewski <github@robertk.io>
2024-06-04 11:15:32 +0100

    less

--- vortex-ipc/src/io/offset.rs
+++ vortex-ipc/src/io/offset.rs
@@ -1,9 +1,8 @@
 use std::future::Future;
-use std::io;
 
 use bytes::BytesMut;
 
-use crate::io::{VortexRead, VortexReadAt};
+use crate::io::VortexReadAt;
 
 /// An adapter that offsets all reads by a fixed amount.
 pub struct OffsetReadAt<R> {
@@ -22,20 +21,11 @@ impl<R: VortexReadAt> VortexReadAt for OffsetReadAt<R> {
         &mut self,
         pos: u64,
         buffer: BytesMut,
-    ) -> impl Future<Output = io::Result<BytesMut>> {
+    ) -> impl Future<Output = std::io::Result<BytesMut>> {
         self.read.read_at_into(pos + self.offset, buffer)
     }
 
     fn performance_hint(&self) -> usize {
         self.read.performance_hint()
-    }
-}
-
-impl<R: VortexReadAt> VortexRead for OffsetReadAt<R> {
-    async fn read_into(&mut self, buffer: BytesMut) -> io::Result<BytesMut> {
-        let buffer_len = buffer.len() as u64;
-        let res = self.read.read_at_into(self.offset, buffer).await?;
-        self.offset += buffer_len;
-        Ok(res)
     }
 }
