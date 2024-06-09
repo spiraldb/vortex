b2eb660ac39cb5792e9f9faca7f6008ea6dcbe29
Nicholas Gates <nick@nickgates.com>
2024-06-09 17:07:56 +0100

    Clean up

--- fastlanez/benches/fastlanes_bitpacking.rs
+++ fastlanez/benches/fastlanes_bitpacking.rs
@@ -3,19 +3,27 @@
 
 use std::mem::{size_of, MaybeUninit};
 
-use arrayref::array_mut_ref;
+use arrayref::{array_mut_ref, array_ref};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use fastlanez::{BitPack, BitPack2};
 
 fn bitpacking(c: &mut Criterion) {
     let mut group = c.benchmark_group("bitpacking");
 
-    group.bench_function("pack 16 -> 3", |b| {
+    group.bench_function("pack 16 -> 3 heap", |b| {
         const WIDTH: usize = 3;
-        let values = [3u16; 1024];
-
+        let values = vec![3u16; 1024];
         let mut packed = vec![0; 128 * WIDTH / size_of::<u16>()];
 
+        b.iter(|| {
+            BitPack2::<WIDTH>::bitpack(array_ref![values, 0, 1024], array_mut_ref![packed, 0, 192]);
+        });
+    });
+
+    group.bench_function("pack 16 -> 3 stack", |b| {
+        const WIDTH: usize = 3;
+        let values = [3u16; 1024];
+        let mut packed = [0; 128 * WIDTH / size_of::<u16>()];
         b.iter(|| {
             BitPack2::<WIDTH>::bitpack(&values, array_mut_ref![packed, 0, 192]);
         });
