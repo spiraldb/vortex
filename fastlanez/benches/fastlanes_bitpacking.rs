84db7a6231bc07938eb43b8904b6b1338995b080
Nicholas Gates <nick@nickgates.com>
2024-06-10 09:24:43 +0100

    BitUnpacking

--- fastlanez/benches/fastlanes_bitpacking.rs
+++ fastlanez/benches/fastlanes_bitpacking.rs
@@ -8,36 +8,51 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use fastlanez::{BitPack, BitPack2};
 
 fn bitpacking(c: &mut Criterion) {
-    let mut group = c.benchmark_group("bitpacking");
+    {
+        let mut group = c.benchmark_group("bit-packing");
+        group.bench_function("pack 16 -> 3 heap", |b| {
+            const WIDTH: usize = 3;
+            let values = vec![3u16; 1024];
+            let mut packed = vec![0; 128 * WIDTH / size_of::<u16>()];
 
-    group.bench_function("pack 16 -> 3 heap", |b| {
-        const WIDTH: usize = 3;
-        let values = vec![3u16; 1024];
-        let mut packed = vec![0; 128 * WIDTH / size_of::<u16>()];
-
-        b.iter(|| {
-            BitPack2::<WIDTH>::bitpack(array_ref![values, 0, 1024], array_mut_ref![packed, 0, 192]);
+            b.iter(|| {
+                BitPack2::<WIDTH>::bitpack(
+                    array_ref![values, 0, 1024],
+                    array_mut_ref![packed, 0, 192],
+                );
+            });
         });
-    });
 
-    group.bench_function("pack 16 -> 3 stack", |b| {
-        const WIDTH: usize = 3;
-        let values = [3u16; 1024];
-        let mut packed = [0; 128 * WIDTH / size_of::<u16>()];
-        b.iter(|| {
-            BitPack2::<WIDTH>::bitpack(&values, array_mut_ref![packed, 0, 192]);
+        group.bench_function("pack 16 -> 3 stack", |b| {
+            const WIDTH: usize = 3;
+            let values = [3u16; 1024];
+            let mut packed = [0; 128 * WIDTH / size_of::<u16>()];
+            b.iter(|| BitPack2::<WIDTH>::bitpack(&values, &mut packed));
         });
-    });
 
-    group.bench_function("old pack 16 -> 3", |b| {
-        const WIDTH: usize = 3;
-        let values = [3u16; 1024];
+        group.bench_function("old pack 16 -> 3", |b| {
+            const WIDTH: usize = 3;
+            let values = [3u16; 1024];
 
-        b.iter(|| {
-            let mut packed = [MaybeUninit::new(0u8); 128 * WIDTH];
-            black_box(BitPack::<WIDTH>::pack(&values, &mut packed));
+            b.iter(|| {
+                let mut packed = [MaybeUninit::new(0u8); 128 * WIDTH];
+                black_box(BitPack::<WIDTH>::pack(&values, &mut packed));
+            });
         });
-    });
+    }
+
+    {
+        let mut group = c.benchmark_group("bit-unpacking");
+        group.bench_function("unpack 16 <- 3 heap", |b| {
+            const WIDTH: usize = 3;
+            let values = [3u16; 1024];
+            let mut packed = [0; 128 * WIDTH / size_of::<u16>()];
+            BitPack2::<WIDTH>::bitpack(&values, &mut packed);
+
+            let mut unpacked = [0u16; 1024];
+            b.iter(|| BitPack2::<WIDTH>::bitunpack(&packed, &mut unpacked));
+        });
+    }
 }
 
 criterion_group!(benches, bitpacking);
--- fastlanez/src/fl.rs
+++ fastlanez/src/fl.rs
@@ -1,7 +1,7 @@
 use std::mem::size_of;
 
 use arrayref::array_mut_ref;
-use num_traits::{PrimInt, Unsigned};
+use num_traits::{One, PrimInt, Unsigned};
 use paste::paste;
 use seq_macro::seq;
 
@@ -39,102 +39,9 @@ macro_rules! seq_type_width {
     ($ident:ident in u64 $body:tt) => {seq!($ident in 0..64 $body);};
 }
 
-impl FastLanes for u16 {}
-
-impl<const W: usize> BitPack2<W> for u16
-where
-    Pred<{ W > 0 }>: Satisfied,
-    Pred<{ W < 8 * size_of::<Self>() }>: Satisfied,
-    [(); 128 * W / size_of::<Self>()]:,
-{
-    fn bitpack(input: &[Self; 1024], output: &mut [Self; 128 * W / size_of::<Self>()]) {
-        let mask = (1 << W) - 1;
-
-        // First we loop over each lane in the virtual 1024 bit word.
-        for i in 0..Self::LANES {
-            let mut tmp: Self = 0;
-
-            // Inlined loop over each of the rows of the lane.
-            seq_type_width!(row in u16 {{
-                let src = input[Self::LANES * row + i] & mask;
-
-                // Shift the src bits into their position in the tmp output variable.
-                if row == 0 {
-                    tmp = src;
-                } else {
-                    tmp |= src << (row * Self::WIDTH) % Self::T;
-                }
-
-                // If the next input value overlaps with the next output, then we
-                // write out the tmp variable and bring forward the remaining bits.
-                let curr_out: usize = (row * Self::WIDTH) / Self::T;
-                let next_out: usize = ((row + 1) * Self::WIDTH) / Self::T;
-                if next_out > curr_out {
-                    output[Self::LANES * curr_out + i] = tmp;
-
-                    let remaining_bits: usize = ((row + 1) * Self::WIDTH) % Self::T;
-                    tmp = src >> Self::WIDTH - remaining_bits;
-                }
-            }});
-        }
-    }
-
-    fn bitunpack(input: &[Self; 128 * W / size_of::<u16>()], output: &mut [Self; 1024]) {
-        let mut src: Self = 0;
-        let mut tmp: Self = 0;
-        let mut base: Self = 0;
-
-        for i in 0..Self::LANES {
-            src = input[i + 0];
-
-            tmp = (src >> 0) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 0)] = tmp;
-
-            tmp = (src >> 3) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 1)] = tmp;
-
-            tmp = (src >> 6) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 2)] = tmp;
-
-            tmp = (src >> 9) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 3)] = tmp;
-
-            tmp = (src >> 12) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 4)] = tmp;
-
-            tmp = (src >> 15) & ((1 << 1) - 1);
-
-            let curr_in: usize = (1 * Self::WIDTH) / Self::T;
-            src = input[i + 64];
-
-            tmp |= ((src) & ((1 << 2) - 1)) << 1;
-            output[i + (Self::LANES * 5)] = tmp;
-            tmp = (src >> 2) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 6)] = tmp;
-            tmp = (src >> 5) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 7)] = tmp;
-            tmp = (src >> 8) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 8)] = tmp;
-            tmp = (src >> 11) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 9)] = tmp;
-            tmp = (src >> 14) & ((1 << 2) - 1);
-
-            let curr_in: usize = (2 * Self::WIDTH) / Self::T;
-            src = input[i + 128];
-            tmp |= ((src) & ((1 << 1) - 1)) << 2;
-            output[i + (Self::LANES * 10)] = tmp;
-            tmp = (src >> 1) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 11)] = tmp;
-            tmp = (src >> 4) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 12)] = tmp;
-            tmp = (src >> 7) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 13)] = tmp;
-            tmp = (src >> 10) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 14)] = tmp;
-            tmp = (src >> 13) & ((1 << 3) - 1);
-            output[i + (Self::LANES * 15)] = tmp;
-        }
-    }
+#[inline]
+fn mask<T: PrimInt + Unsigned + One>(width: usize) -> T {
+    (T::one() << width) - T::one()
 }
 
 // We need to use a macro instead of generic impl since we have to know the bit-width of T ahead
@@ -151,10 +58,6 @@ macro_rules! impl_bitpacking {
                 Pred<{ W < 8 * size_of::<Self>() }>: Satisfied,
                 [(); 128 * W / size_of::<Self>()]:,
             {
-                fn bitunpack(input: &[Self; 128 * W / size_of::<Self>()], output: &mut [Self; 1024]) {
-                    todo!()
-                }
-
                 #[inline(never)] // Makes it easier to disassemble and validate ASM.
                 #[allow(unused_assignments)] // Inlined loop gives unused assignment on final iteration
                 fn bitpack<'a>(input: &[Self; 1024], output: &mut [Self; 128 * W / size_of::<Self>()]) {
@@ -162,9 +65,11 @@ macro_rules! impl_bitpacking {
 
                     // First we loop over each lane in the virtual 1024 bit word.
                     for i in 0..Self::LANES {
-                        let mut tmp: $T = 0;
+                        let mut tmp: Self = 0;
 
-                        // Inlined loop over each of the rows of the lane.
+                        // Loop over each of the rows of the lane.
+                        // Inlining this loop means all branches are known at compile time and
+                        // the code is auto-vectorized for SIMD execution.
                         seq_type_width!(row in $T {{
                             let src = input[Self::LANES * row + i] & mask;
 
@@ -177,14 +82,49 @@ macro_rules! impl_bitpacking {
 
                             // If the next input value overlaps with the next output, then we
                             // write out the tmp variable and bring forward the remaining bits.
-                            let curr_out: usize = (row * Self::WIDTH) / Self::T;
-                            let next_out: usize = ((row + 1) * Self::WIDTH) / Self::T;
-                            if next_out > curr_out {
-                                output[Self::LANES * curr_out + i] = tmp;
+                            let curr_pos: usize = (row * Self::WIDTH) / Self::T;
+                            let next_pos: usize = ((row + 1) * Self::WIDTH) / Self::T;
+                            if next_pos > curr_pos {
+                                output[Self::LANES * curr_pos + i] = tmp;
 
                                 let remaining_bits: usize = ((row + 1) * Self::WIDTH) % Self::T;
                                 tmp = src >> Self::WIDTH - remaining_bits;
                             }
+                        }});
+                    }
+                }
+
+                #[inline(never)] // Makes it easier to disassemble and validate ASM.
+                fn bitunpack(input: &[Self; 128 * W / size_of::<Self>()], output: &mut [Self; 1024]) {
+                    for i in 0..Self::LANES {
+                        let mut src = input[i];
+                        let mut tmp: Self;
+
+                        seq_type_width!(row in $T {{
+                            let curr_pos: usize = (row * Self::WIDTH) / Self::T;
+                            let next_pos = ((row + 1) * Self::WIDTH) / Self::T;
+
+                            let shift = (row * Self::WIDTH) % Self::T;
+
+                            if next_pos > curr_pos {
+                                // Consume some bits from the curr input, the remainder are in the next input
+                                let remaining_bits = ((row + 1) * Self::WIDTH) % Self::T;
+                                let current_bits = Self::WIDTH - remaining_bits;
+                                tmp = (src >> shift) & mask::<Self>(current_bits);
+
+                                if next_pos < Self::WIDTH {
+                                    // Load the next input value
+                                    src = input[Self::LANES * next_pos + i];
+                                    // Consume the remaining bits from the next input value.
+                                    tmp |= (src & mask::<Self>(remaining_bits)) << current_bits;
+                                }
+                            } else {
+                                // Otherwise, just grab W bits from the src value
+                                tmp = (src >> shift) & mask::<Self>(Self::WIDTH);
+                            }
+
+                            // Write out the unpacked value
+                            output[(Self::LANES * row) + i] = tmp;
                         }});
                     }
                 }
@@ -224,7 +164,7 @@ impl TryBitPack2 for u16 {
 }
 
 impl_bitpacking!(u8);
-// impl_bitpacking!(u16);
+impl_bitpacking!(u16);
 impl_bitpacking!(u32);
 impl_bitpacking!(u64);
 
