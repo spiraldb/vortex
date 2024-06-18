38252e22e8e27ecfd338626de398c3a827bd9236
Robert Kruszewski <github@robertk.io>
2024-06-18 17:16:30 +0100

    Upgrade py03 to new bound api

--- pyvortex/src/dtype.rs
+++ pyvortex/src/dtype.rs
@@ -1,7 +1,7 @@
 use arrow::datatypes::{DataType, Field};
 use arrow::pyarrow::FromPyArrow;
 use pyo3::types::PyType;
-use pyo3::{pyclass, pymethods, Py, PyAny, PyResult, Python};
+use pyo3::{pyclass, pymethods, Bound, Py, PyAny, PyResult, Python};
 use vortex::arrow::FromArrowType;
 use vortex_dtype::DType;
 
@@ -28,7 +28,7 @@ impl PyDType {
 
     #[classmethod]
     fn from_pyarrow(
-        cls: &PyType,
+        cls: &Bound<PyType>,
         #[pyo3(from_py_with = "import_arrow_dtype")] arrow_dtype: DataType,
         nullable: bool,
     ) -> PyResult<Py<Self>> {
@@ -39,6 +39,6 @@ impl PyDType {
     }
 }
 
-fn import_arrow_dtype(obj: &PyAny) -> PyResult<DataType> {
-    DataType::from_pyarrow(obj)
+fn import_arrow_dtype(obj: &Bound<PyAny>) -> PyResult<DataType> {
+    DataType::from_pyarrow_bound(obj)
 }
