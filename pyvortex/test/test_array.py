df06c5fbbbb730a079c3bbecafe0265b6b08d8dc
Andrew Duffy <andreweduffy@gmail.com>
2024-06-26 13:27:38 -0400

    fix pytests

--- pyvortex/test/test_array.py
+++ pyvortex/test/test_array.py
@@ -18,10 +18,9 @@ def test_varbin_array_round_trip():
 
 def test_varbin_array_take():
     a = vortex.encode(pa.array(["a", "b", "c", "d"]))
-    # TODO(ngates): ensure we correctly round-trip to a string and not large_string
     assert a.take(vortex.encode(pa.array([0, 2]))).to_pyarrow().combine_chunks() == pa.array(
         ["a", "c"],
-        type=pa.large_utf8(),
+        type=pa.utf8(),
     )
 
 
