1d5a68b167ca37ff5da473627b04e3f1d4cb9c1d
Robert Kruszewski <github@robertk.io>
2024-06-26 13:24:02 +0100

    Add SearchSortedFn for ConstantArray (#417)

--- vortex-array/src/array/constant/compute.rs
+++ vortex-array/src/array/constant/compute.rs
@@ -1,12 +1,15 @@
+use std::cmp::Ordering;
+
 use vortex_error::VortexResult;
 use vortex_scalar::Scalar;
 
 use crate::array::constant::ConstantArray;
+use crate::compute::search_sorted::{SearchResult, SearchSortedFn, SearchSortedSide};
 use crate::compute::slice::SliceFn;
 use crate::compute::take::TakeFn;
 use crate::compute::unary::scalar_at::ScalarAtFn;
 use crate::compute::ArrayCompute;
-use crate::{Array, IntoArray};
+use crate::{Array, ArrayTrait, IntoArray};
 
 impl ArrayCompute for ConstantArray {
     fn scalar_at(&self) -> Option<&dyn ScalarAtFn> {
@@ -14,6 +17,10 @@ impl ArrayCompute for ConstantArray {
     }
 
     fn slice(&self) -> Option<&dyn SliceFn> {
+        Some(self)
+    }
+
+    fn search_sorted(&self) -> Option<&dyn SearchSortedFn> {
         Some(self)
     }
 
@@ -37,5 +44,51 @@ impl TakeFn for ConstantArray {
 impl SliceFn for ConstantArray {
     fn slice(&self, start: usize, stop: usize) -> VortexResult<Array> {
         Ok(Self::new(self.scalar().clone(), stop - start).into_array())
+    }
+}
+
+impl SearchSortedFn for ConstantArray {
+    fn search_sorted(&self, value: &Scalar, side: SearchSortedSide) -> VortexResult<SearchResult> {
+        match self.scalar().partial_cmp(value).unwrap_or(Ordering::Less) {
+            Ordering::Greater => Ok(SearchResult::NotFound(0)),
+            Ordering::Less => Ok(SearchResult::NotFound(self.len())),
+            Ordering::Equal => match side {
+                SearchSortedSide::Left => Ok(SearchResult::Found(0)),
+                SearchSortedSide::Right => Ok(SearchResult::Found(self.len())),
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::array::constant::ConstantArray;
+    use crate::compute::search_sorted::{search_sorted, SearchResult, SearchSortedSide};
+    use crate::IntoArray;
+
+    #[test]
+    pub fn search() {
+        let cst = ConstantArray::new(42, 5000).into_array();
+        assert_eq!(
+            search_sorted(&cst, 33, SearchSortedSide::Left).unwrap(),
+            SearchResult::NotFound(0)
+        );
+        assert_eq!(
+            search_sorted(&cst, 55, SearchSortedSide::Left).unwrap(),
+            SearchResult::NotFound(5000)
+        );
+    }
+
+    #[test]
+    pub fn search_equals() {
+        let cst = ConstantArray::new(42, 5000).into_array();
+        assert_eq!(
+            search_sorted(&cst, 42, SearchSortedSide::Left).unwrap(),
+            SearchResult::Found(0)
+        );
+        assert_eq!(
+            search_sorted(&cst, 42, SearchSortedSide::Right).unwrap(),
+            SearchResult::Found(5000)
+        );
     }
 }
