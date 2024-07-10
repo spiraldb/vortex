8b56dbb30aa68c4a5a04b9e04f838fbee74899d3
Andrew Duffy <andreweduffy@gmail.com>
2024-07-09 23:21:23 -0400

    strip suffix

--- xtask/src/tpch.rs
+++ xtask/src/tpch.rs
@@ -1,5 +1,6 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
+use itertools::Itertools;
 use xshell::{cmd, Shell};
 
 #[derive(clap::Subcommand)]
@@ -35,8 +36,27 @@ impl TpchCommand {
     )
                     .run()?;
 
+                for file in sh.read_dir(tpch_path)? {
+                    println!(
+                        "stripping trailing pipe from {:?}",
+                        file.file_name().unwrap()
+                    );
+                    strip_suffix(&sh, &file)?;
+                }
+
                 Ok(())
             }
         }
     }
+}
+
+fn strip_suffix<P: AsRef<Path>>(sh: &Shell, path: P) -> anyhow::Result<()> {
+    let stripped = sh
+        .read_file(&path)?
+        .lines()
+        .map(|line| line.strip_suffix("|").unwrap())
+        .join("\n");
+    sh.write_file(&path, stripped.as_bytes())?;
+
+    Ok(())
 }
