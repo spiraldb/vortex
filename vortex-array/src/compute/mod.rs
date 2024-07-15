1148978d129966eab69f2cb6e0d3fccd2e273b40
Andrew Duffy <andreweduffy@gmail.com>
2024-07-15 17:55:16 +0100

    docs + public

--- vortex-array/src/compute/mod.rs
+++ vortex-array/src/compute/mod.rs
@@ -20,51 +20,82 @@ use self::unary::scalar_subtract::SubtractScalarFn;
 use crate::compute::filter::FilterFn;
 
 pub mod compare;
-mod filter;
+pub mod filter;
 pub mod filter_indices;
 pub mod search_sorted;
 pub mod slice;
 pub mod take;
 pub mod unary;
 
-/// Trait providing compute functions on top of
+/// Trait providing compute functions on top of Vortex arrays.
 pub trait ArrayCompute {
+    /// Implemented for arrays that can be casted to different types.
+    ///
+    /// See: [CastFn].
     fn cast(&self) -> Option<&dyn CastFn> {
         None
     }
 
+    /// Binary operator implementation for arrays against other arrays.
+    ///
+    ///See: [CompareFn].
     fn compare(&self) -> Option<&dyn CompareFn> {
         None
     }
 
+    /// Array function that returns new arrays a non-null value is repeated across runs of nulls.
+    ///
+    /// See: [FillForwardFn].
     fn fill_forward(&self) -> Option<&dyn FillForwardFn> {
         None
     }
 
+    /// Filtering function on arrays of predicates.
+    ///
+    /// See: [FilterFn].
     fn filter(&self) -> Option<&dyn FilterFn> {
         None
     }
 
+    /// Filter indices based on a disjunctive normal form relational expression.
+    /// TODO(aduffy): remove this function and push implementation into vortex-datafusion.
+    #[deprecated]
     fn filter_indices(&self) -> Option<&dyn FilterIndicesFn> {
         None
     }
 
+    /// Single item indexing on Vortex arrays.
+    ///
+    /// See: [ScalarAtFn].
     fn scalar_at(&self) -> Option<&dyn ScalarAtFn> {
         None
     }
 
+    /// Broadcast subtraction of scalar from Vortex array.
+    ///
+    /// See: [SubtractScalarFn].
     fn subtract_scalar(&self) -> Option<&dyn SubtractScalarFn> {
         None
     }
 
+    /// Perform a search over an ordered array.
+    ///
+    /// See: [SearchSortedFn].
     fn search_sorted(&self) -> Option<&dyn SearchSortedFn> {
         None
     }
 
+    /// Perform zero-copy slicing of an array.
+    ///
+    /// See: [SliceFn].
     fn slice(&self) -> Option<&dyn SliceFn> {
         None
     }
 
+    /// Take a set of indices from an array. This often forces allocations and decoding of
+    /// the receiver.
+    ///
+    /// See: [TakeFn].
     fn take(&self) -> Option<&dyn TakeFn> {
         None
     }
