6b03048c521f4bb1133708903e35d93ae591728f
Andrew Duffy <andreweduffy@gmail.com>
2024-07-22 11:46:59 -0400

    cast chunks

--- vortex-array/src/array/chunked/compute/mod.rs
+++ vortex-array/src/array/chunked/compute/mod.rs
@@ -1,9 +1,11 @@
+use vortex_dtype::DType;
 use vortex_error::VortexResult;
 use vortex_scalar::Scalar;
 
 use crate::array::chunked::ChunkedArray;
-use crate::compute::unary::{scalar_at, ScalarAtFn, SubtractScalarFn};
+use crate::compute::unary::{scalar_at, try_cast, CastFn, ScalarAtFn, SubtractScalarFn};
 use crate::compute::{ArrayCompute, SliceFn, TakeFn};
+use crate::{Array, IntoArray};
 
 mod slice;
 mod take;
@@ -30,5 +32,16 @@ impl ScalarAtFn for ChunkedArray {
     fn scalar_at(&self, index: usize) -> VortexResult<Scalar> {
         let (chunk_index, chunk_offset) = self.find_chunk_idx(index);
         scalar_at(&self.chunk(chunk_index).unwrap(), chunk_offset)
+    }
+}
+
+impl CastFn for ChunkedArray {
+    fn cast(&self, dtype: &DType) -> VortexResult<Array> {
+        let mut cast_chunks = Vec::new();
+        for chunk in self.chunks() {
+            cast_chunks.push(try_cast(&chunk, dtype)?);
+        }
+
+        Ok(ChunkedArray::try_new(cast_chunks, dtype.clone())?.into_array())
     }
 }
