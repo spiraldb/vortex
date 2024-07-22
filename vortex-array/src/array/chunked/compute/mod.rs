939d30ba5db39b758a0d2364afe1edd1aaebde90
Andrew Duffy <andreweduffy@gmail.com>
2024-07-22 11:57:52 -0400

    add test

--- vortex-array/src/array/chunked/compute/mod.rs
+++ vortex-array/src/array/chunked/compute/mod.rs
@@ -11,6 +11,10 @@ mod slice;
 mod take;
 
 impl ArrayCompute for ChunkedArray {
+    fn cast(&self) -> Option<&dyn CastFn> {
+        Some(self)
+    }
+
     fn scalar_at(&self) -> Option<&dyn ScalarAtFn> {
         Some(self)
     }
@@ -43,5 +47,49 @@ impl CastFn for ChunkedArray {
         }
 
         Ok(ChunkedArray::try_new(cast_chunks, dtype.clone())?.into_array())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use vortex_dtype::{DType, Nullability, PType};
+
+    use crate::array::chunked::ChunkedArray;
+    use crate::array::primitive::PrimitiveArray;
+    use crate::compute::unary::try_cast;
+    use crate::validity::Validity;
+    use crate::{IntoArray, IntoArrayVariant};
+
+    #[test]
+    fn test_cast_chunked() {
+        let arr0 = PrimitiveArray::from_vec(vec![0u32, 1], Validity::NonNullable).into_array();
+        let arr1 = PrimitiveArray::from_vec(vec![2u32, 3], Validity::NonNullable).into_array();
+
+        let chunked = ChunkedArray::try_new(
+            vec![arr0, arr1],
+            DType::Primitive(PType::U32, Nullability::NonNullable),
+        )
+        .unwrap()
+        .into_array();
+
+        // Two levels of chunking, just to be fancy.
+        let root = ChunkedArray::try_new(
+            vec![chunked],
+            DType::Primitive(PType::U32, Nullability::NonNullable),
+        )
+        .unwrap()
+        .into_array();
+
+        assert_eq!(
+            try_cast(
+                &root,
+                &DType::Primitive(PType::U64, Nullability::NonNullable)
+            )
+            .unwrap()
+            .into_primitive()
+            .unwrap()
+            .into_maybe_null_slice::<u64>(),
+            vec![0u64, 1, 2, 3],
+        );
     }
 }
