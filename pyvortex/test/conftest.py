b8ac152aad469ff4b0af5c7773b096cd0504ae8f
Robert Kruszewski <github@robertk.io>
2024-07-31 14:31:17 +0100

    locked

--- pyvortex/test/conftest.py
+++ pyvortex/test/conftest.py
@@ -8,4 +8,4 @@ logging.basicConfig(level=logging.DEBUG)
 def pytest_sessionstart():
     """Pytest plugin to trigger maturin builds before running tests."""
     working_dir = pathlib.Path(__file__).parent.parent
-    subprocess.check_call(["maturin", "develop", "-v", "--skip-install", "--locked", "--frozen"], cwd=working_dir)
+    subprocess.check_call(["maturin", "develop", "-v", "--skip-install", "--locked"], cwd=working_dir)
