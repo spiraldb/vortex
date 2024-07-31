05df4bfcfc896e7c25b155f6a305720702ab0879
Robert Kruszewski <github@robertk.io>
2024-07-31 16:57:33 +0100

    less

--- pyvortex/test/conftest.py
+++ pyvortex/test/conftest.py
@@ -1,3 +1,11 @@
 import logging
+import pathlib
+import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
+
+
+def pytest_sessionstart():
+    """Pytest plugin to trigger maturin builds before running tests."""
+    working_dir = pathlib.Path(__file__).parent.parent
+    subprocess.check_call(["maturin", "develop", "--skip-install"], cwd=working_dir)
