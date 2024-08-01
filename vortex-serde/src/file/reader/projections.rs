bdc65a73405bef3b4b265d1681f7d38a5e88c2a8
Adam Gutglick <adam@spiraldb.com>
2024-08-01 13:41:51 +0100

    Add identity projection

--- vortex-serde/src/file/reader/projections.rs
+++ vortex-serde/src/file/reader/projections.rs
@@ -1,22 +1,18 @@
-#[derive(Clone)]
-pub struct Projection {
-    indices: Vec<usize>,
+#[derive(Clone, Default)]
+pub enum Projection {
+    #[default]
+    All,
+    Partial(Vec<usize>),
 }
 
 impl Projection {
     pub fn new(indices: impl AsRef<[usize]>) -> Self {
-        Projection {
-            indices: Vec::from(indices.as_ref()),
-        }
-    }
-
-    pub fn indices(&self) -> &[usize] {
-        self.indices.as_ref()
+        Self::Partial(Vec::from(indices.as_ref()))
     }
 }
 
 impl From<Vec<usize>> for Projection {
     fn from(indices: Vec<usize>) -> Self {
-        Self { indices }
+        Self::Partial(indices)
     }
 }
