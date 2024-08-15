9a7f6015d0ccba20e981ead81caa200744c6f65d
Robert Kruszewski <github@robertk.io>
2024-08-15 16:54:52 +0100

    Remove dead code after disk and in memory table provider unification

--- vortex-datafusion/src/expr.rs
+++ vortex-datafusion/src/expr.rs
@@ -5,12 +5,9 @@ use std::fmt::Debug;
 use std::sync::Arc;
 
 use arrow_schema::{Schema, SchemaRef};
-use datafusion::optimizer::simplify_expressions::ExprSimplifier;
 use datafusion_common::tree_node::{TreeNode, TreeNodeRecursion};
-use datafusion_common::{DataFusionError, Result as DFResult, ToDFSchema};
-use datafusion_expr::execution_props::ExecutionProps;
-use datafusion_expr::simplify::SimplifyContext;
-use datafusion_expr::{and, lit, Expr, Operator as DFOperator};
+use datafusion_common::{DataFusionError, Result as DFResult};
+use datafusion_expr::Operator as DFOperator;
 use datafusion_physical_expr::PhysicalExpr;
 use vortex::array::{ConstantArray, StructArray};
 use vortex::compute::compare;
@@ -21,33 +18,6 @@ use vortex_expr::Operator;
 use vortex_scalar::Scalar;
 
 use crate::scalar::dfvalue_to_scalar;
-
-/// Convert a set of expressions into a single AND expression.
-///
-/// # Returns
-///
-/// If conversion is successful, the result will be a
-/// [binary expression node][datafusion_expr::Expr::BinaryExpr] containing the conjunction.
-pub(crate) fn make_conjunction(exprs: impl AsRef<[Expr]>) -> DFResult<Expr> {
-    Ok(exprs
-        .as_ref()
-        .iter()
-        .fold(lit(true), |conj, elem| and(conj, elem.clone())))
-}
-
-/// Simplify an expression using DataFusion's builtin analysis passes.
-///
-/// This encapsulates common optimizations like constant folding and eliminating redundant
-/// expressions, e.g. `value AND true`.
-pub(crate) fn simplify_expr(expr: &Expr, schema: SchemaRef) -> DFResult<Expr> {
-    let schema = schema.to_dfschema_ref()?;
-
-    let props = ExecutionProps::new();
-    let context = SimplifyContext::new(&props).with_schema(schema);
-    let simplifier = ExprSimplifier::new(context);
-
-    simplifier.simplify(expr.clone())
-}
 
 pub trait VortexPhysicalExpr: Debug + Send + Sync {
     fn evaluate(&self, array: &Array) -> VortexResult<Array>;
@@ -205,29 +175,4 @@ pub(crate) fn extract_columns_from_expr(
     }
 
     Ok(predicate_projection)
-}
-
-#[cfg(test)]
-mod test {
-    use std::sync::Arc;
-
-    use arrow_schema::{DataType, Field, Schema};
-    use datafusion_expr::{col, lit};
-
-    use super::*;
-
-    #[test]
-    fn test_conjunction_simplify() {
-        let schema = Arc::new(Schema::new(vec![
-            Field::new("int_col", DataType::Int32, false),
-            Field::new("bool_col", DataType::Boolean, false),
-        ]));
-
-        let exprs = vec![col("int_col").gt_eq(lit(4)), col("bool_col").is_true()];
-
-        assert_eq!(
-            simplify_expr(&make_conjunction(&exprs).unwrap(), schema).unwrap(),
-            and(col("int_col").gt_eq(lit(4)), col("bool_col").is_true())
-        );
-    }
 }
