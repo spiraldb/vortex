cfd0943fa4a4201ad245a7cb39e9012c77c93299
Andrew Duffy <a10y@users.noreply.github.com>
2024-08-19 10:44:49 -0400

    Add FSST encoding (#645)
    This is just the FSSTArray, leaning on the shiny new `fsst-rs` crate
    we're publishing.
    
    This does not implement the encoding compressor, will do that in a
    follow-on.

---
+++ encodings/fsst/src/lib.rs
@@ -0,0 +1,15 @@
+//! An array that uses the [Fast Static Symbol Table][fsst] compression scheme
+//! to compress string arrays.
+//!
+//! FSST arrays can generally compress string data up to 2x through the use of
+//! string tables. The string table is static for an entire array, and occupies
+//! up to 2048 bytes of buffer space. Thus, FSST is only worth reaching for when
+//! dealing with larger arrays of potentially hundreds of kilobytes or more.
+//!
+//! [fsst]: https://www.vldb.org/pvldb/vol13/p2649-boncz.pdf
+
+mod array;
+mod canonical;
+mod compute;
+
+pub use array::*;
