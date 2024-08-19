7bf50a4ab3c0049db30777caaa1fe0116c069f0a
Andrew Duffy <andreweduffy@gmail.com>
2024-08-19 09:56:45 -0400

    break out tests using rstest

--- encodings/fsst/tests/fsst_tests.rs
+++ encodings/fsst/tests/fsst_tests.rs
@@ -2,16 +2,24 @@
 
 use arrow_array::builder::BinaryBuilder;
 use fsst::{Compressor, Symbol};
+use rstest::{fixture, rstest};
 use vortex::array::{BoolArray, PrimitiveArray};
 use vortex::arrow::FromArrowArray;
 use vortex::compute::unary::scalar_at;
 use vortex::compute::{filter, slice, take};
 use vortex::validity::Validity;
-use vortex::{ArrayDType, ArrayDef, IntoArray};
+use vortex::{Array, ArrayDType, ArrayDef, IntoArray};
 use vortex_dtype::{DType, Nullability, PType};
 use vortex_fsst::{FSSTArray, FSST};
 
-fn build_fsst_utf8_array() -> FSSTArray {
+macro_rules! assert_nth_scalar {
+    ($arr:expr, $n:expr, $expected:expr) => {
+        assert_eq!(scalar_at(&$arr, $n).unwrap(), $expected.try_into().unwrap());
+    };
+}
+
+#[fixture]
+fn fsst_array() -> Array {
     let compressor = Compressor::train(
         "The Greeks never said that the limit could not he overstepped. \
         They said it existed and that whoever dared to exceed it was mercilessly struck down. \
@@ -50,100 +58,82 @@ fn build_fsst_utf8_array() -> FSSTArray {
     let codes = codes.finish();
     let codes_array = vortex::Array::from_arrow(&codes, false);
 
-    FSSTArray::try_new(
+    let fsst_array = FSSTArray::try_new(
         DType::Utf8(Nullability::NonNullable),
         symbols_array,
         codes_array,
     )
-    .expect("building from parts must succeed")
-}
-
-macro_rules! assert_nth_scalar {
-    ($arr:expr, $n:expr, $expected:expr) => {
-        assert_eq!(scalar_at(&$arr, $n).unwrap(), $expected.try_into().unwrap());
-    };
-}
-
-#[test]
-fn test_compute() {
-    let fsst_array = build_fsst_utf8_array().into_array();
+    .expect("building from parts must succeed");
 
     assert_eq!(fsst_array.len(), 3);
 
-    //
-    // ScalarAtFn
-    //
-    {
-        assert_nth_scalar!(
-            fsst_array,
-            0,
-            "The Greeks never said that the limit could not he overstepped"
-        );
-        assert_nth_scalar!(
-            fsst_array,
-            1,
-            "They said it existed and that whoever dared to exceed it was mercilessly struck down"
-        );
-        assert_nth_scalar!(
-            fsst_array,
-            2,
-            "Nothing in present history can contradict them"
-        );
-    }
+    fsst_array.into_array()
+}
 
-    //
-    // SliceFn
-    //
-    {
-        let fsst_sliced = slice(&fsst_array, 1, 3).unwrap();
-        assert_eq!(fsst_sliced.encoding().id(), FSST::ENCODING.id());
-        assert_eq!(fsst_sliced.len(), 2);
-        assert_nth_scalar!(
-            fsst_sliced,
-            0,
-            "They said it existed and that whoever dared to exceed it was mercilessly struck down"
-        );
-        assert_nth_scalar!(
-            fsst_sliced,
-            1,
-            "Nothing in present history can contradict them"
-        );
-    }
+#[rstest]
+fn test_scalar_at(fsst_array: Array) {
+    assert_nth_scalar!(
+        fsst_array,
+        0,
+        "The Greeks never said that the limit could not he overstepped"
+    );
+    assert_nth_scalar!(
+        fsst_array,
+        1,
+        "They said it existed and that whoever dared to exceed it was mercilessly struck down"
+    );
+    assert_nth_scalar!(
+        fsst_array,
+        2,
+        "Nothing in present history can contradict them"
+    );
+}
 
-    //
-    // TakeFn
-    //
-    {
-        let indices = PrimitiveArray::from_vec(vec![0, 2], Validity::NonNullable).into_array();
-        let fsst_taken = take(&fsst_array, &indices).unwrap();
-        assert_eq!(fsst_taken.len(), 2);
-        assert_nth_scalar!(
-            fsst_taken,
-            0,
-            "The Greeks never said that the limit could not he overstepped"
-        );
-        assert_nth_scalar!(
-            fsst_taken,
-            1,
-            "Nothing in present history can contradict them"
-        );
-    }
+#[rstest]
+fn test_slice(fsst_array: Array) {
+    let fsst_sliced = slice(&fsst_array, 1, 3).unwrap();
+    assert_eq!(fsst_sliced.encoding().id(), FSST::ENCODING.id());
+    assert_eq!(fsst_sliced.len(), 2);
+    assert_nth_scalar!(
+        fsst_sliced,
+        0,
+        "They said it existed and that whoever dared to exceed it was mercilessly struck down"
+    );
+    assert_nth_scalar!(
+        fsst_sliced,
+        1,
+        "Nothing in present history can contradict them"
+    );
+}
 
-    //
-    // FilterFn
-    //
+#[rstest]
+fn test_take(fsst_array: Array) {
+    let indices = PrimitiveArray::from_vec(vec![0, 2], Validity::NonNullable).into_array();
+    let fsst_taken = take(&fsst_array, &indices).unwrap();
+    assert_eq!(fsst_taken.len(), 2);
+    assert_nth_scalar!(
+        fsst_taken,
+        0,
+        "The Greeks never said that the limit could not he overstepped"
+    );
+    assert_nth_scalar!(
+        fsst_taken,
+        1,
+        "Nothing in present history can contradict them"
+    );
+}
 
-    {
-        let predicate =
-            BoolArray::from_vec(vec![false, true, false], Validity::NonNullable).into_array();
+#[rstest]
+fn test_filter(fsst_array: Array) {
+    let predicate =
+        BoolArray::from_vec(vec![false, true, false], Validity::NonNullable).into_array();
 
-        let fsst_filtered = filter(&fsst_array, &predicate).unwrap();
-        assert_eq!(fsst_filtered.encoding().id(), FSST::ENCODING.id());
-        assert_eq!(fsst_filtered.len(), 1);
-        assert_nth_scalar!(
-            fsst_filtered,
-            0,
-            "They said it existed and that whoever dared to exceed it was mercilessly struck down"
-        );
-    }
+    let fsst_filtered = filter(&fsst_array, &predicate).unwrap();
+    assert_eq!(fsst_filtered.encoding().id(), FSST::ENCODING.id());
+    assert_eq!(fsst_filtered.len(), 1);
+    assert_nth_scalar!(
+        fsst_filtered,
+        0,
+        "They said it existed and that whoever dared to exceed it was mercilessly struck down"
+    );
 }
