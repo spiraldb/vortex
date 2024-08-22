61955e82cb4a44b7086217046718ca952ca06f3b
Daniel King <dan@spiraldb.com>
2024-08-22 14:56:23 +0100

    impl Display for Utf8Scalar and BinaryScalar
    Partially addresses #670.

--- vortex-scalar/src/display.rs
+++ vortex-scalar/src/display.rs
@@ -4,6 +4,8 @@ use vortex_dtype::{match_each_native_ptype, DType};
 
 use crate::bool::BoolScalar;
 use crate::primitive::PrimitiveScalar;
+use crate::utf8::Utf8Scalar;
+use crate::binary::BinaryScalar;
 use crate::Scalar;
 
 impl Display for Scalar {
@@ -30,8 +32,43 @@ impl Display for Scalar {
                     Some(v) => write!(f, "{}", v),
                 }
             }),
-            DType::Utf8(_) => todo!(),
-            DType::Binary(_) => todo!(),
+            DType::Utf8(_) => {
+                match Utf8Scalar::try_from(self)
+                    .map_err(|err| {
+                        debug_assert!(
+                            false,
+                            "Failed to parse utf8 from scalar with DType Utf8Scalar: {}",
+                            err
+                        );
+                        std::fmt::Error
+                    })?
+                    .value()
+                {
+                    None => write!(f, "null"),
+                    Some(bs) => f.write_str(bs.as_str()),
+                }
+            }
+            DType::Binary(_) => {
+                match BinaryScalar::try_from(self)
+                    .map_err(|err| {
+                        debug_assert!(
+                            false,
+                            "Failed to parse binary from scalar with DType Binary: {}",
+                            err
+                        );
+                        std::fmt::Error
+                    })?
+                    .value()
+                {
+                    None => write!(f, "null"),
+                    Some(buf) => {
+                        for &b in buf.as_slice() {
+                            write!(f, "{:x}", b)?;
+                        };
+                        Ok(())
+                    },
+                }
+            },
             DType::Struct(..) => todo!(),
             DType::List(..) => todo!(),
             DType::Extension(..) => todo!(),
@@ -42,10 +79,47 @@ impl Display for Scalar {
 #[cfg(test)]
 mod tests {
     use crate::Scalar;
+    use vortex_dtype::DType;
+    use vortex_dtype::Nullability::{NonNullable, Nullable};
+    use vortex_dtype::PType;
+    use vortex_buffer::Buffer;
 
     #[test]
-    fn display() {
-        let scalar = Scalar::from(false);
-        assert_eq!(format!("{}", scalar), "false");
+    fn display_bool() {
+        assert_eq!(format!("{}", Scalar::from(false)), "false");
+        assert_eq!(format!("{}", Scalar::from(true)), "true");
+        assert_eq!(format!("{}", Scalar::null(DType::Bool(Nullable))), "null");
+    }
+
+    #[test]
+    fn display_primitive() {
+        assert_eq!(format!("{}", Scalar::from(0_u8)), "0");
+        assert_eq!(format!("{}", Scalar::from(255_u8)), "255");
+
+        assert_eq!(format!("{}", Scalar::from(0_u16)), "0");
+        assert_eq!(format!("{}", Scalar::from(!0_u16)), "65535");
+
+        assert_eq!(format!("{}", Scalar::from(0_u32)), "0");
+        assert_eq!(format!("{}", Scalar::from(!0_u32)), "4294967295");
+
+        assert_eq!(format!("{}", Scalar::from(0_u64)), "0");
+        assert_eq!(format!("{}", Scalar::from(!0_u64)), "18446744073709551615");
+
+        assert_eq!(format!("{}", Scalar::null(DType::Primitive(PType::U8, Nullable))), "null");
+    }
+
+    #[test]
+    fn display_utf8() {
+        assert_eq!(format!("{}", Scalar::from("Hello World!")), "Hello World!");
+        assert_eq!(format!("{}", Scalar::null(DType::Utf8(Nullable))), "null");
+    }
+
+    #[test]
+    fn display_binary() {
+        assert_eq!(
+            format!("{}", Scalar::binary(Buffer::from("Hello World!".as_bytes()), NonNullable)),
+            "48656c6c6f20576f726c6421"
+        );
+        assert_eq!(format!("{}", Scalar::null(DType::Binary(Nullable))), "null");
     }
 }
