09678ee5ecec6c1177c0e1ce796957132f3c1444
Dan King <dan@spiraldb.com>
2024-09-03 16:20:58 -0400

    Prefer "arrow" to "pyarrow" (#715)
    This matches PyTorch, Ray, datatable, and polars:
    -
    https://pytorch.org/torcharrow/beta/generated/torcharrow.from_arrow.html
    - https://docs.ray.io/en/latest/data/api/doc/ray.data.from_arrow.html
    - https://datatable.readthedocs.io/en/latest/api/frame/to_arrow.html
    -
    https://docs.pola.rs/api/python/stable/reference/dataframe/api/polars.DataFrame.to_arrow.html

--- pyvortex/src/array.rs
+++ pyvortex/src/array.rs
@@ -201,7 +201,7 @@ impl PyArray {
 
 #[pymethods]
 impl PyArray {
-    fn to_pyarrow(self_: PyRef<'_, Self>) -> PyResult<Bound<PyAny>> {
+    fn to_arrow(self_: PyRef<'_, Self>) -> PyResult<Bound<PyAny>> {
         vortex_arrow::export_array(self_.py(), &self_.inner)
     }
 
