5a8b9e4a1ba444d0e88d08ab94302b13df572bc2
Adam Gutglick <adam@spiraldb.com>
2024-09-04 16:03:14 +0100

    small rename

--- vortex-array/src/elementwise.rs
+++ vortex-array/src/elementwise.rs
@@ -50,84 +50,84 @@ pub trait UnaryFn {
 }
 
 // TODO(adamgs): Turn into a macro, or just have some intermediate adapter struct
-pub fn flat_array_iter<I: NativePType>(array: &Array) -> Box<dyn Iterator<Item = I>> {
+pub fn flat_array_iter<N: NativePType>(array: &Array) -> Box<dyn Iterator<Item = N>> {
     match array.dtype().as_ptype().unwrap() {
         PType::U8 => Box::new(
             array
                 .with_dyn(|a| a.as_primitive_array_unchecked().u8_iter())
                 .unwrap()
                 .flatten()
-                .map(|o| I::from(o.unwrap_or_default()).unwrap()),
+                .map(|o| N::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::U16 => Box::new(
             array
                 .with_dyn(|a| a.as_primitive_array_unchecked().u16_iter())
                 .unwrap()
                 .flatten()
-                .map(|o| I::from(o.unwrap_or_default()).unwrap()),
+                .map(|o| N::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::U32 => Box::new(
             array
                 .with_dyn(|a| a.as_primitive_array_unchecked().u32_iter())
                 .unwrap()
                 .flatten()
-                .map(|o| I::from(o.unwrap_or_default()).unwrap()),
+                .map(|o| N::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::U64 => Box::new(
             array
                 .with_dyn(|a| a.as_primitive_array_unchecked().u64_iter())
                 .unwrap()
                 .flatten()
-                .map(|o| I::from(o.unwrap_or_default()).unwrap()),
+                .map(|o| N::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::I8 => Box::new(
             array
                 .with_dyn(|a| a.as_primitive_array_unchecked().i8_iter())
                 .unwrap()
                 .flatten()
-                .map(|o| I::from(o.unwrap_or_default()).unwrap()),
+                .map(|o| N::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::I16 => Box::new(
             array
                 .with_dyn(|a| a.as_primitive_array_unchecked().i16_iter())
                 .unwrap()
                 .flatten()
-                .map(|o| I::from(o.unwrap_or_default()).unwrap()),
+                .map(|o| N::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::I32 => Box::new(
             array
                 .with_dyn(|a| a.as_primitive_array_unchecked().i32_iter())
                 .unwrap()
                 .flatten()
-                .map(|o| I::from(o.unwrap_or_default()).unwrap()),
+                .map(|o| N::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::I64 => Box::new(
             array
                 .with_dyn(|a| a.as_primitive_array_unchecked().i64_iter())
                 .unwrap()
                 .flatten()
-                .map(|o| I::from(o.unwrap_or_default()).unwrap()),
+                .map(|o| N::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::F16 => Box::new(
             array
                 .with_dyn(|a| a.as_primitive_array_unchecked().u64_iter())
                 .unwrap()
                 .flatten()
-                .map(|o| I::from(o.unwrap_or_default()).unwrap()),
+                .map(|o| N::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::F32 => Box::new(
             array
                 .with_dyn(|a| a.as_primitive_array_unchecked().f32_iter())
                 .unwrap()
                 .flatten()
-                .map(|o| I::from(o.unwrap_or_default()).unwrap()),
+                .map(|o| N::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::F64 => Box::new(
             array
                 .with_dyn(|a| a.as_primitive_array_unchecked().f64_iter())
                 .unwrap()
                 .flatten()
-                .map(|o| I::from(o.unwrap_or_default()).unwrap()),
+                .map(|o| N::from(o.unwrap_or_default()).unwrap()),
         ),
     }
 }
