2079385accad7776bebc22cc9e93bd807a99bda3
Adam Gutglick <adam@spiraldb.com>
2024-09-04 14:45:31 +0100

    Fix validity

--- vortex-array/src/elementwise.rs
+++ vortex-array/src/elementwise.rs
@@ -3,7 +3,6 @@ use vortex_error::{vortex_bail, VortexError, VortexResult};
 use vortex_scalar::Scalar;
 
 use crate::array::PrimitiveArray;
-use crate::validity::Validity;
 use crate::{Array, ArrayDType, IntoArray};
 
 pub enum OtherValue {
@@ -91,90 +90,105 @@ impl BinaryFn for PrimitiveArray {
         let lhs = self.maybe_null_slice::<I>();
         let mut output = Vec::with_capacity(self.len());
 
-        match other {
-            OtherValue::Scalar(s) => {
-                let s = I::try_from(s)?;
+        let validity = match other {
+            OtherValue::Scalar(ref s) => {
+                let s = I::try_from(s.clone())?;
                 for v in lhs {
                     output.push(f(*v, s));
                 }
+                self.validity()
             }
-            OtherValue::Array(a) => {
+            OtherValue::Array(ref a) => {
                 let rhs_iter = flat_array_iter::<I>(a);
                 for (l, r) in lhs.iter().copied().zip(rhs_iter) {
                     output.push(f(l, r));
                 }
-            }
-        }
 
-        Ok(PrimitiveArray::from_vec(output, Validity::AllValid).into_array())
+                let rhs = a.with_dyn(|a| a.logical_validity().into_validity());
+                self.validity().and(rhs)?
+            }
+        };
+
+        Ok(PrimitiveArray::from_vec(output, validity).into_array())
     }
 }
 
 // TODO(adamgs): Turn into a macro, or just have some intermediate adapter struct
-fn flat_array_iter<I: NativePType>(a: Array) -> Box<dyn Iterator<Item = I>> {
-    match a.dtype().as_ptype().unwrap() {
+fn flat_array_iter<I: NativePType>(array: &Array) -> Box<dyn Iterator<Item = I>> {
+    match array.dtype().as_ptype().unwrap() {
         PType::U8 => Box::new(
-            a.with_dyn(|a| a.as_primitive_array_unchecked().u8_iter())
+            array
+                .with_dyn(|a| a.as_primitive_array_unchecked().u8_iter())
                 .unwrap()
                 .flatten()
                 .map(|o| I::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::U16 => Box::new(
-            a.with_dyn(|a| a.as_primitive_array_unchecked().u16_iter())
+            array
+                .with_dyn(|a| a.as_primitive_array_unchecked().u16_iter())
                 .unwrap()
                 .flatten()
                 .map(|o| I::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::U32 => Box::new(
-            a.with_dyn(|a| a.as_primitive_array_unchecked().u32_iter())
+            array
+                .with_dyn(|a| a.as_primitive_array_unchecked().u32_iter())
                 .unwrap()
                 .flatten()
                 .map(|o| I::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::U64 => Box::new(
-            a.with_dyn(|a| a.as_primitive_array_unchecked().u64_iter())
+            array
+                .with_dyn(|a| a.as_primitive_array_unchecked().u64_iter())
                 .unwrap()
                 .flatten()
                 .map(|o| I::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::I8 => Box::new(
-            a.with_dyn(|a| a.as_primitive_array_unchecked().i8_iter())
+            array
+                .with_dyn(|a| a.as_primitive_array_unchecked().i8_iter())
                 .unwrap()
                 .flatten()
                 .map(|o| I::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::I16 => Box::new(
-            a.with_dyn(|a| a.as_primitive_array_unchecked().i16_iter())
+            array
+                .with_dyn(|a| a.as_primitive_array_unchecked().i16_iter())
                 .unwrap()
                 .flatten()
                 .map(|o| I::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::I32 => Box::new(
-            a.with_dyn(|a| a.as_primitive_array_unchecked().i32_iter())
+            array
+                .with_dyn(|a| a.as_primitive_array_unchecked().i32_iter())
                 .unwrap()
                 .flatten()
                 .map(|o| I::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::I64 => Box::new(
-            a.with_dyn(|a| a.as_primitive_array_unchecked().i64_iter())
+            array
+                .with_dyn(|a| a.as_primitive_array_unchecked().i64_iter())
                 .unwrap()
                 .flatten()
                 .map(|o| I::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::F16 => Box::new(
-            a.with_dyn(|a| a.as_primitive_array_unchecked().u64_iter())
+            array
+                .with_dyn(|a| a.as_primitive_array_unchecked().u64_iter())
                 .unwrap()
                 .flatten()
                 .map(|o| I::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::F32 => Box::new(
-            a.with_dyn(|a| a.as_primitive_array_unchecked().f32_iter())
+            array
+                .with_dyn(|a| a.as_primitive_array_unchecked().f32_iter())
                 .unwrap()
                 .flatten()
                 .map(|o| I::from(o.unwrap_or_default()).unwrap()),
         ),
         PType::F64 => Box::new(
-            a.with_dyn(|a| a.as_primitive_array_unchecked().f64_iter())
+            array
+                .with_dyn(|a| a.as_primitive_array_unchecked().f64_iter())
                 .unwrap()
                 .flatten()
                 .map(|o| I::from(o.unwrap_or_default()).unwrap()),
@@ -186,6 +200,7 @@ fn flat_array_iter<I: NativePType>(a: Array) -> Box<dyn Iterator<Item = I>> {
 mod tests {
 
     use super::*;
+    use crate::validity::Validity;
 
     #[test]
     fn elementwise_example() {
