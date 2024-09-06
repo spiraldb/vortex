8b8926b0f6651ea5d54366a006673f8079e34c53
Adam Gutglick <adam@spiraldb.com>
2024-09-06 15:17:02 +0100

    Better implementation for `Validity::and` (#758)
    Previous implementation was actually buggy because it generated nullable
    arrays.

--- vortex-array/src/validity.rs
+++ vortex-array/src/validity.rs
@@ -1,3 +1,5 @@
+use std::ops::BitAnd;
+
 use arrow_buffer::{BooleanBuffer, BooleanBufferBuilder, NullBuffer};
 use serde::{Deserialize, Serialize};
 use vortex_dtype::{DType, Nullability};
@@ -5,7 +7,7 @@ use vortex_error::{vortex_bail, VortexResult};
 
 use crate::array::BoolArray;
 use crate::compute::unary::scalar_at_unchecked;
-use crate::compute::{and, filter, slice, take};
+use crate::compute::{filter, slice, take};
 use crate::stats::ArrayStatistics;
 use crate::{Array, IntoArray, IntoArrayVariant};
 
@@ -143,7 +145,7 @@ impl Validity {
 
     /// Logically & two Validity values of the same length
     pub fn and(self, rhs: Validity) -> VortexResult<Validity> {
-        let validity = match (self, rhs) {
+        let validity = match (&self, &rhs) {
             // Any `AllInvalid` makes the output all invalid values
             (Validity::AllInvalid, _) | (_, Validity::AllInvalid) => Validity::AllInvalid,
             // All truthy values on one side, which makes no effect on an `Array` variant
@@ -157,7 +159,15 @@ impl Validity {
             | (Validity::AllValid, Validity::NonNullable)
             | (Validity::AllValid, Validity::AllValid) => Validity::AllValid,
             // Here we actually have to do some work
-            (Validity::Array(lhs), Validity::Array(rhs)) => Validity::Array(and(&lhs, &rhs)?),
+            (Validity::Array(lhs), Validity::Array(rhs)) => {
+                let lhs = BoolArray::try_from(lhs)?;
+                let rhs = BoolArray::try_from(rhs)?;
+
+                let lhs = lhs.boolean_buffer();
+                let rhs = rhs.boolean_buffer();
+
+                Validity::from(lhs.bitand(&rhs))
+            }
         };
 
         Ok(validity)
