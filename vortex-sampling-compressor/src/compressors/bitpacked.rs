45560d4ddb82ffd401178fc4cdd42ddf3a8d3d06
Will Manning <will@willmanning.io>
2024-10-09 14:57:39 -0400

    stuff and things

--- vortex-sampling-compressor/src/compressors/bitpacked.rs
+++ vortex-sampling-compressor/src/compressors/bitpacked.rs
@@ -3,11 +3,10 @@ use std::collections::HashSet;
 use vortex::array::PrimitiveArray;
 use vortex::encoding::EncodingRef;
 use vortex::stats::ArrayStatistics;
-use vortex::{Array, ArrayDef, IntoArray};
+use vortex::{Array, IntoArray};
 use vortex_error::{vortex_err, vortex_panic, VortexResult};
 use vortex_fastlanes::{
-    bitpack, count_exceptions, find_best_bit_width, find_min_patchless_bit_width, gather_patches,
-    BitPacked, BitPackedArray, BitPackedEncoding,
+    bitpack, count_exceptions, find_best_bit_width, find_min_patchless_bit_width, gather_patches, BitPackedArray, BitPackedEncoding,
 };
 
 use crate::compressors::{CompressedArray, CompressionTree, EncodingCompressor};
