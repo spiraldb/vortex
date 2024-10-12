3ae9c57c62dc8ac0cb2476c1d70358f3362911e7
Robert Kruszewski <github@robertk.io>
2024-10-12 01:05:02 +0100

    Fix writing of Chunk of Struct arrays

--- vortex-serde/src/layouts/write/writer.rs
+++ vortex-serde/src/layouts/write/writer.rs
@@ -14,14 +14,13 @@ use crate::layouts::read::{ChunkedLayoutSpec, ColumnLayoutSpec};
 use crate::layouts::write::footer::Footer;
 use crate::layouts::write::layouts::{FlatLayout, Layout, NestedLayout};
 use crate::layouts::MAGIC_BYTES;
-use crate::stream_writer::ChunkOffsets;
 use crate::MessageWriter;
 
 pub struct LayoutWriter<W> {
     msgs: MessageWriter<W>,
 
     dtype: Option<DType>,
-    column_chunks: Vec<ChunkOffsets>,
+    column_chunks: Vec<BatchOffsets>,
 }
 
 impl<W: VortexWrite> LayoutWriter<W> {
@@ -83,16 +82,17 @@ impl<W: VortexWrite> LayoutWriter<W> {
         let column_row_offset = self
             .column_chunks
             .get(column_idx)
-            .and_then(|c| c.row_offsets.last())
+            .and_then(|c| c.row_offsets.back())
             .copied()
             .unwrap_or(0u64);
         let mut byte_offsets = vec![self.msgs.tell()];
-        let mut row_offsets = vec![column_row_offset];
+        let mut row_offsets = VecDeque::new();
+        row_offsets.push_front(column_row_offset);
 
         while let Some(chunk) = stream.try_next().await? {
-            row_offsets.push(
+            row_offsets.push_back(
                 row_offsets
-                    .last()
+                    .back()
                     .map(|off| off + chunk.len() as u64)
                     .ok_or_else(|| vortex_err!("Row offsets should be initialized with a value"))?,
             );
@@ -100,16 +100,15 @@ impl<W: VortexWrite> LayoutWriter<W> {
             byte_offsets.push(self.msgs.tell());
         }
 
-        if let Some(chunk) = self.column_chunks.get_mut(column_idx) {
+        if let Some(batches) = self.column_chunks.get_mut(column_idx) {
             // Remove last entry from the list as it would be the same as first entry of next chunk
-            byte_offsets.truncate(byte_offsets.len() - 1);
-            row_offsets.truncate(row_offsets.len() - 1);
+            row_offsets.pop_front();
 
-            chunk.byte_offsets.extend(byte_offsets);
-            chunk.row_offsets.extend(row_offsets);
+            batches.batch_byte_offsets.push(byte_offsets);
+            batches.row_offsets.extend(row_offsets);
         } else {
             self.column_chunks
-                .push(ChunkOffsets::new(byte_offsets, row_offsets));
+                .push(BatchOffsets::new(vec![byte_offsets], row_offsets));
         }
 
         Ok(())
@@ -118,18 +117,23 @@ impl<W: VortexWrite> LayoutWriter<W> {
     async fn write_metadata_arrays(&mut self) -> VortexResult<NestedLayout> {
         let mut column_layouts = VecDeque::with_capacity(self.column_chunks.len());
         for mut chunk in mem::take(&mut self.column_chunks) {
-            let len = chunk.byte_offsets.len() - 1;
             let mut chunks: VecDeque<Layout> = chunk
-                .byte_offsets
+                .batch_byte_offsets
                 .iter()
-                .zip(chunk.byte_offsets.iter().skip(1))
-                .map(|(begin, end)| Layout::Flat(FlatLayout::new(*begin, *end)))
+                .flat_map(|byte_offsets| {
+                    byte_offsets
+                        .iter()
+                        .zip(byte_offsets.iter().skip(1))
+                        .map(|(begin, end)| Layout::Flat(FlatLayout::new(*begin, *end)))
+                })
                 .collect();
+            let len = chunk.row_offsets.len() - 1;
             chunk.row_offsets.truncate(len);
 
+            let offset_vec: Vec<u64> = chunk.row_offsets.into();
             let metadata_array = StructArray::try_new(
                 ["row_offset".into()].into(),
-                vec![chunk.row_offsets.into_array()],
+                vec![offset_vec.into_array()],
                 len,
                 Validity::NonNullable,
             )?;
@@ -176,6 +180,21 @@ impl<W: VortexWrite> LayoutWriter<W> {
         w.write_all(footer_offset.to_le_bytes()).await?;
         w.write_all(MAGIC_BYTES).await?;
         Ok(w)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct BatchOffsets {
+    pub batch_byte_offsets: Vec<Vec<u64>>,
+    pub row_offsets: VecDeque<u64>,
+}
+
+impl BatchOffsets {
+    pub fn new(batch_byte_offsets: Vec<Vec<u64>>, row_offsets: VecDeque<u64>) -> Self {
+        Self {
+            batch_byte_offsets,
+            row_offsets,
+        }
     }
 }
 
