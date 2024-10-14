06298f821e9aed6663cd3a1519fbcceea62aa5f3
Dan King <dan@spiraldb.com>
2024-10-14 14:44:29 -0400

    fix: Vortex (de)compress benchmarks read/write a Layout (#1024)
    Before this change, we unfairly compared Vortex Array (de)compression to
    Parquet-ZSTD Array compression-and-write.

--- bench-vortex/benches/compress_noci.rs
+++ bench-vortex/benches/compress_noci.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 use std::time::Duration;
 use std::{env, fs};
 
-use arrow_array::RecordBatch;
+use arrow_array::{ArrayRef, RecordBatch};
 use arrow_schema::Schema;
 use bench_vortex::data_downloads::BenchmarkDataset;
 use bench_vortex::public_bi_data::BenchmarkDatasets;
@@ -12,18 +12,21 @@ use bench_vortex::public_bi_data::PBIDataset::*;
 use bench_vortex::taxi_data::taxi_data_parquet;
 use bench_vortex::tpch::dbgen::{DBGen, DBGenOptions};
 use bench_vortex::{fetch_taxi_data, tpch};
-use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion, Throughput};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use futures::TryStreamExt;
 use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
 use parquet::arrow::ArrowWriter;
 use parquet::basic::{Compression, ZstdLevel};
 use parquet::file::properties::WriterProperties;
 use regex::Regex;
+use tokio::runtime::Runtime;
 use vortex::array::{ChunkedArray, StructArray};
 use vortex::{Array, ArrayDType, IntoArray, IntoCanonical};
 use vortex_dtype::field::Field;
+use vortex_error::VortexResult;
 use vortex_sampling_compressor::compressors::fsst::FSSTCompressor;
-use vortex_sampling_compressor::SamplingCompressor;
-use vortex_serde::layouts::LayoutWriter;
+use vortex_sampling_compressor::{SamplingCompressor, ALL_COMPRESSORS_CONTEXT};
+use vortex_serde::layouts::{LayoutContext, LayoutDeserializer, LayoutReaderBuilder, LayoutWriter};
 
 #[derive(serde::Serialize)]
 struct GenericBenchmarkResults<'a> {
@@ -56,15 +59,7 @@ fn chunked_to_vec_record_batch(chunked: ChunkedArray) -> (Vec<RecordBatch>, Arc<
     (batches, schema)
 }
 
-fn parquet_compress(
-    batches: Vec<RecordBatch>,
-    schema: Arc<Schema>,
-    compression: Compression,
-) -> usize {
-    parquet_compress_into(batches, schema, compression, &mut Vec::new())
-}
-
-fn parquet_compress_into(
+fn parquet_compress_write(
     batches: Vec<RecordBatch>,
     schema: Arc<Schema>,
     compression: Compression,
@@ -84,7 +79,7 @@ fn parquet_compress_into(
     n_bytes
 }
 
-fn parquet_decompress_from(buf: bytes::Bytes) -> usize {
+fn parquet_decompress_read(buf: bytes::Bytes) -> usize {
     let builder = ParquetRecordBatchReaderBuilder::try_new(buf).unwrap();
     let reader = builder.build().unwrap();
     let mut nbytes = 0;
@@ -94,27 +89,63 @@ fn parquet_decompress_from(buf: bytes::Bytes) -> usize {
     nbytes
 }
 
-fn parquet_written_size(array: &Array, compression: Compression) -> usize {
+fn parquet_compressed_written_size(array: &Array, compression: Compression) -> usize {
     let chunked = ChunkedArray::try_from(array).unwrap();
     let (batches, schema) = chunked_to_vec_record_batch(chunked);
-    parquet_compress(batches, schema, compression)
+    parquet_compress_write(batches, schema, compression, &mut Vec::new())
 }
 
-fn vortex_written_size(array: &Array) -> u64 {
-    async fn run(array: &Array) -> u64 {
-        let buf = Cursor::new(Vec::new());
-        let mut writer = LayoutWriter::new(buf);
+fn vortex_compress_write(
+    runtime: &Runtime,
+    compressor: &SamplingCompressor<'_>,
+    array: &Array,
+    buf: &mut Vec<u8>,
+) -> VortexResult<u64> {
+    async fn async_write(array: &Array, cursor: &mut Cursor<&mut Vec<u8>>) -> VortexResult<()> {
+        let mut writer = LayoutWriter::new(cursor);
 
-        writer = writer.write_array_columns(array.clone()).await.unwrap();
-        let buf = writer.finalize().await.unwrap();
-        buf.position()
+        writer = writer.write_array_columns(array.clone()).await?;
+        writer.finalize().await?;
+        Ok(())
     }
 
-    tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(run(array))
+    let compressed = compressor.compress(array, None)?.into_array();
+    let mut cursor = Cursor::new(buf);
+
+    runtime.block_on(async_write(&compressed, &mut cursor))?;
+
+    Ok(cursor.position())
+}
+
+fn vortex_decompress_read(runtime: &Runtime, buf: Arc<Vec<u8>>) -> VortexResult<ArrayRef> {
+    async fn async_read(buf: Arc<Vec<u8>>) -> VortexResult<Array> {
+        let builder: LayoutReaderBuilder<_> = LayoutReaderBuilder::new(
+            buf,
+            LayoutDeserializer::new(
+                ALL_COMPRESSORS_CONTEXT.clone(),
+                LayoutContext::default().into(),
+            ),
+        );
+
+        let stream = builder.build().await?;
+        let dtype = stream.schema().clone().into();
+        let vecs: Vec<Array> = stream.try_collect().await?;
+
+        ChunkedArray::try_new(vecs, dtype).map(|e| e.into())
+    }
+
+    runtime
+        .block_on(async_read(buf))?
+        .into_canonical()?
+        .into_arrow()
+}
+
+fn vortex_compressed_written_size(
+    runtime: &Runtime,
+    compressor: &SamplingCompressor<'_>,
+    array: &Array,
+) -> VortexResult<u64> {
+    vortex_compress_write(runtime, compressor, array, &mut Vec::new())
 }
 
 fn benchmark_compress<F, U>(
@@ -129,6 +160,10 @@ fn benchmark_compress<F, U>(
     U: AsRef<Array>,
 {
     ensure_dir_exists("benchmarked-files").unwrap();
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
     let uncompressed = make_uncompressed();
     let uncompressed_size = uncompressed.as_ref().nbytes();
     let mut compressed_size = 0;
@@ -140,9 +175,10 @@ fn benchmark_compress<F, U>(
         measurement_time.map(|t| group.measurement_time(t));
         group.bench_function(bench_name, |b| {
             b.iter_with_large_drop(|| {
-                let compressed =
-                    black_box(compressor.compress(uncompressed.as_ref(), None).unwrap());
-                compressed_size = compressed.nbytes();
+                compressed_size = black_box(
+                    vortex_compressed_written_size(&runtime, compressor, uncompressed.as_ref())
+                        .unwrap(),
+                );
             });
         });
         group.finish();
@@ -156,26 +192,18 @@ fn benchmark_compress<F, U>(
         group.bench_function(bench_name, |b| {
             let chunked = ChunkedArray::try_from(uncompressed.as_ref()).unwrap();
             let (batches, schema) = chunked_to_vec_record_batch(chunked);
+
             b.iter_with_large_drop(|| {
-                black_box(parquet_compress(
+                black_box(parquet_compress_write(
                     batches.clone(),
                     schema.clone(),
                     Compression::ZSTD(ZstdLevel::default()),
+                    &mut Vec::new(),
                 ));
             });
         });
         group.finish();
     }
-
-    let (compressed_array, path) = compressor
-        .compress(uncompressed.as_ref(), None)
-        .unwrap()
-        .into_parts();
-    println!("compression path: {:#?}", path);
-    println!(
-        "compression ratio: {}",
-        compressed_size as f64 / uncompressed_size as f64
-    );
 
     {
         let mut group = c.benchmark_group("decompress time");
@@ -183,13 +211,12 @@ fn benchmark_compress<F, U>(
         group.throughput(Throughput::Bytes(uncompressed_size as u64));
         measurement_time.map(|t| group.measurement_time(t));
         group.bench_function(bench_name, |b| {
-            b.iter_batched(
-                || compressed_array.clone(),
-                |compressed| {
-                    black_box(compressed.into_canonical().unwrap().into_arrow().unwrap());
-                },
-                BatchSize::PerIteration,
-            );
+            let mut buf = Vec::new();
+            vortex_compress_write(&runtime, compressor, uncompressed.as_ref(), &mut buf).unwrap();
+            let arc = Arc::new(buf);
+            b.iter_with_large_drop(|| {
+                black_box(vortex_decompress_read(&runtime, arc.clone()).unwrap());
+            });
         });
         group.finish();
     }
@@ -203,7 +230,7 @@ fn benchmark_compress<F, U>(
             let chunked = ChunkedArray::try_from(uncompressed.as_ref()).unwrap();
             let (batches, schema) = chunked_to_vec_record_batch(chunked);
             let mut buf = Vec::new();
-            parquet_compress_into(
+            parquet_compress_write(
                 batches.clone(),
                 schema.clone(),
                 Compression::ZSTD(ZstdLevel::default()),
@@ -211,7 +238,7 @@ fn benchmark_compress<F, U>(
             );
             let bytes = bytes::Bytes::from(buf);
             b.iter_with_large_drop(|| {
-                black_box(parquet_decompress_from(bytes.clone()));
+                black_box(parquet_decompress_read(bytes.clone()));
             });
         });
         group.finish();
@@ -222,14 +249,10 @@ fn benchmark_compress<F, U>(
         .map(|x| Regex::new(&x).unwrap().is_match(bench_name))
         .unwrap_or(false)
     {
-        let vortex_nbytes = vortex_written_size(
-            &compressor
-                .compress(uncompressed.as_ref(), None)
-                .unwrap()
-                .into_array(),
-        );
+        let vortex_nbytes =
+            vortex_compressed_written_size(&runtime, compressor, uncompressed.as_ref()).unwrap();
 
-        let parquet_zstd_nbytes = parquet_written_size(
+        let parquet_zstd_nbytes = parquet_compressed_written_size(
             uncompressed.as_ref(),
             Compression::ZSTD(ZstdLevel::default()),
         );
