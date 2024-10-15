1cb4e56180300a6ad39bdf7f2a47e33535edb5df
Will Manning <will@willmanning.io>
2024-10-14 21:33:21 -0400

    feat: canonicalize array if taking large number of indices

--- vortex-array/src/compute/take.rs
+++ vortex-array/src/compute/take.rs
@@ -1,7 +1,7 @@
 use log::info;
-use vortex_error::{vortex_err, VortexResult};
+use vortex_error::{vortex_bail, vortex_err, VortexResult};
 
-use crate::{Array, IntoCanonical};
+use crate::{Array, ArrayDType as _, IntoCanonical as _};
 
 pub trait TakeFn {
     fn take(&self, indices: &Array) -> VortexResult<Array>;
@@ -10,6 +10,24 @@ pub trait TakeFn {
 pub fn take(array: impl AsRef<Array>, indices: impl AsRef<Array>) -> VortexResult<Array> {
     let array = array.as_ref();
     let indices = indices.as_ref();
+
+    if !indices.dtype().is_int() || indices.dtype().is_nullable() {
+        vortex_bail!(
+            "Take indices must be a non-nullable integer type, got {}",
+            indices.dtype()
+        );
+    }
+
+    // If the indices are large enough, it's faster to canonicalize the array and then take.
+    if indices.len() >= array.len() {
+        do_take(&Array::from(array.clone().into_canonical()?), indices)
+    } else {
+        do_take(array, indices)
+    }
+}
+
+#[inline]
+fn do_take(array: &Array, indices: &Array) -> VortexResult<Array> {
     array.with_dyn(|a| {
         if let Some(take) = a.take() {
             return take.take(indices);
