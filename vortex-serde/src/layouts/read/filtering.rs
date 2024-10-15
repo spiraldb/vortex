8778d2cc33e41bbe44a57987718ff71bf0801f5a
Robert Kruszewski <github@robertk.io>
2024-10-15 18:46:37 +0100

    filter

--- vortex-serde/src/layouts/read/filtering.rs
+++ vortex-serde/src/layouts/read/filtering.rs
@@ -2,13 +2,17 @@ use std::collections::HashSet;
 use std::fmt::Debug;
 use std::sync::Arc;
 
+use arrow_buffer::BooleanBuffer;
+use vortex::array::BoolArray;
 use vortex::compute::and;
 use vortex::stats::ArrayStatistics;
-use vortex::Array;
+use vortex::validity::Validity;
+use vortex::{Array, ArrayLen, IntoArray, IntoArrayVariant};
 use vortex_dtype::field::Field;
 use vortex_error::{VortexExpect, VortexResult};
 use vortex_expr::{split_conjunction, VortexExpr};
 
+use crate::layouts::null_as_false;
 use crate::layouts::read::filter_project::filter_project;
 
 #[derive(Debug, Clone)]
@@ -35,14 +39,18 @@ impl RowFilter {
             .evaluate(target)?;
         for expr in filter_iter {
             if mask.statistics().compute_true_count().unwrap_or_default() == 0 {
-                return Ok(mask);
+                return BoolArray::try_new(
+                    BooleanBuffer::new_unset(target.len()),
+                    Validity::AllInvalid,
+                )
+                .map(IntoArray::into_array);
             }
 
             let new_mask = expr.evaluate(target)?;
             mask = and(new_mask, mask)?;
         }
 
-        Ok(mask)
+        null_as_false(mask.into_bool()?)
     }
 
     /// Returns a set of all referenced fields in the underlying filter
