d2b89d91e457a67cadd0156c04352b021626de02
Dan King <dan@spiraldb.com>
2024-10-17 12:38:19 -0400

    feat: teach Array to_arrow_table (#1070)
    This function will be used for interop with Polars, DuckDB, and others,
    so I need it extracted from the `to_pandas` method.

--- pyvortex/python/vortex/encoding.py
+++ pyvortex/python/vortex/encoding.py
@@ -9,6 +9,53 @@ Array = _encoding.Array
 compress = _encoding.compress
 
 
+def empty_arrow_table(schema: pyarrow.Schema) -> pyarrow.Table:
+    return pyarrow.Table.from_arrays([[] for _ in schema], schema=schema)
+
+
+def arrow_table_from_struct_array(array: pyarrow.StructArray | pyarrow.ChunkedArray) -> pyarrow.Table:
+    if len(array) == 0:
+        return empty_arrow_table(pyarrow.schema(array.type))
+    return pyarrow.Table.from_struct_array(array)
+
+
+def _Array_to_arrow_table(self: _encoding.Array) -> pyarrow.Table:
+    """Construct an Arrow table from this Vortex array.
+
+    Warning
+    -------
+
+    Only struct-typed arrays can be converted to Arrow tables.
+
+    Returns
+    -------
+
+    :class:`pyarrow.Table`
+
+    Examples
+    --------
+
+    >>> array = vortex.encoding.array([
+    ...     {'name': 'Joseph', 'age': 25},
+    ...     {'name': 'Narendra', 'age': 31},
+    ...     {'name': 'Angela', 'age': 33},
+    ...     {'name': 'Mikhail', 'age': 57},
+    ... ])
+    >>> array.to_arrow_table()
+    pyarrow.Table
+    age: int64
+    name: string
+    ----
+    age: [[25,31,33,57]]
+    name: [["Joseph","Narendra","Angela","Mikhail"]]
+
+    """
+    return arrow_table_from_struct_array(self.to_arrow())
+
+
+Array.to_arrow_table = _Array_to_arrow_table
+
+
 def _Array_to_pandas(self: _encoding.Array):
     """Construct a Pandas dataframe from this Vortex array.
 
@@ -42,17 +89,7 @@ def _Array_to_pandas(self: _encoding.Array):
     Lift the struct fields to the top-level in the dataframe:
 
     """
-    return table_from_struct_array(self.to_arrow()).to_pandas(types_mapper=pandas.ArrowDtype)
-
-
-def empty_table(schema: pyarrow.Schema) -> pyarrow.Table:
-    return pyarrow.Table.from_arrays([[] for _ in schema], schema=schema)
-
-
-def table_from_struct_array(array: pyarrow.StructArray | pyarrow.ChunkedArray):
-    if len(array) == 0:
-        return empty_table(pyarrow.schema(array.type))
-    return pyarrow.Table.from_struct_array(array)
+    return self.to_arrow_table().to_pandas(types_mapper=pandas.ArrowDtype)
 
 
 Array.to_pandas = _Array_to_pandas
