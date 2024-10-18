abb1ee601dcfb2eb8fa4ae7c06ab9448e42a686a
Andrew Duffy <andreweduffy@gmail.com>
2024-10-17 21:52:12 -0400

    canonicalize test

--- vortex-array/src/array/constant/canonical.rs
+++ vortex-array/src/array/constant/canonical.rs
@@ -1,8 +1,8 @@
 use std::iter;
 
 use arrow_buffer::BooleanBuffer;
-use vortex_dtype::{match_each_native_ptype, Nullability, PType};
-use vortex_error::{vortex_bail, VortexExpect, VortexResult};
+use vortex_dtype::{match_each_native_ptype, DType, Nullability, PType};
+use vortex_error::{vortex_bail, VortexResult};
 use vortex_scalar::{BinaryScalar, BoolScalar, Utf8Scalar};
 
 use crate::array::constant::ConstantArray;
@@ -36,35 +36,22 @@ impl IntoCanonical for ConstantArray {
 
         if let Ok(s) = Utf8Scalar::try_from(scalar) {
             let value = s.value();
-            let const_value = value.as_ref().map(|v| v.as_str());
+            let const_value = value.as_ref().map(|v| v.as_bytes());
 
-            let canonical = match validity.nullability() {
-                Nullability::NonNullable => VarBinViewArray::from_iter_str(iter::repeat_n(
-                    const_value
-                        .vortex_expect("null Utf8Scalar value for non-nullable ConstantArray"),
-                    self.len(),
-                )),
-                Nullability::Nullable => {
-                    VarBinViewArray::from_iter_nullable_str(iter::repeat_n(const_value, self.len()))
-                }
-            };
-            return Ok(Canonical::VarBinView(canonical));
+            return Ok(Canonical::VarBinView(VarBinViewArray::from_iter(
+                iter::repeat(const_value).take(self.len()),
+                DType::Utf8(validity.nullability()),
+            )));
         }
 
         if let Ok(b) = BinaryScalar::try_from(scalar) {
             let value = b.value();
             let const_value = value.as_ref().map(|v| v.as_slice());
 
-            let canonical = match validity.nullability() {
-                Nullability::NonNullable => VarBinViewArray::from_iter_bin(iter::repeat_n(
-                    const_value.vortex_expect("null BinaryScalar for non-nullable ConstantArray"),
-                    self.len(),
-                )),
-                Nullability::Nullable => {
-                    VarBinViewArray::from_iter_nullable_bin(iter::repeat_n(const_value, self.len()))
-                }
-            };
-            return Ok(Canonical::VarBinView(canonical));
+            return Ok(Canonical::VarBinView(VarBinViewArray::from_iter(
+                iter::repeat(const_value).take(self.len()),
+                DType::Binary(validity.nullability()),
+            )));
         }
 
         if let Ok(ptype) = PType::try_from(scalar.dtype()) {
@@ -77,5 +64,30 @@ impl IntoCanonical for ConstantArray {
         }
 
         vortex_bail!("Unsupported scalar type {}", self.dtype())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::array::ConstantArray;
+    use crate::compute::unary::scalar_at;
+    use crate::IntoCanonical;
+
+    #[test]
+    fn test_canonicalize_const_str() {
+        let const_array = ConstantArray::new("four".to_string(), 4);
+
+        // Check all values correct.
+        let canonical = const_array
+            .into_canonical()
+            .unwrap()
+            .into_varbinview()
+            .unwrap();
+
+        assert_eq!(canonical.len(), 4);
+
+        for i in 0..=3 {
+            assert_eq!(scalar_at(&canonical, i).unwrap(), "four".into(),);
+        }
     }
 }
