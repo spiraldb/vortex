e8793d6ec4c26163a8a60345507dc961b260c928
Daniel King <dan@spiraldb.com>
2024-10-24 16:29:11 -0400

    update

--- docs2/rust/doc/trait.impl/arrow_array/record_batch/trait.RecordBatchReader.js
+++ docs2/rust/doc/trait.impl/arrow_array/record_batch/trait.RecordBatchReader.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["vortex_serde",[["impl&lt;'a, R: <a class=\"trait\" href=\"vortex_serde/io/trait.VortexReadAt.html\" title=\"trait vortex_serde::io::VortexReadAt\">VortexReadAt</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> + 'static&gt; RecordBatchReader for <a class=\"struct\" href=\"vortex_serde/layouts/struct.VortexRecordBatchReader.html\" title=\"struct vortex_serde::layouts::VortexRecordBatchReader\">VortexRecordBatchReader</a>&lt;'a, R&gt;"]]]]);
+    var implementors = Object.fromEntries([["vortex_serde",[["impl&lt;'a, R, AR&gt; RecordBatchReader for <a class=\"struct\" href=\"vortex_serde/layouts/struct.VortexRecordBatchReader.html\" title=\"struct vortex_serde::layouts::VortexRecordBatchReader\">VortexRecordBatchReader</a>&lt;'a, R, AR&gt;<div class=\"where\">where\n    R: <a class=\"trait\" href=\"vortex_serde/io/trait.VortexReadAt.html\" title=\"trait vortex_serde::io::VortexReadAt\">VortexReadAt</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> + 'static,\n    AR: <a class=\"trait\" href=\"vortex_serde/layouts/trait.AsyncRuntime.html\" title=\"trait vortex_serde::layouts::AsyncRuntime\">AsyncRuntime</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[535]}
+//{"start":57,"fragment_lengths":[735]}
